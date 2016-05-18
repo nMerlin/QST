f6f64078a0d69c4b5b3aaaa8bfcd80884368cfad
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2016-05-18 18:21:19 +0200

    Changed and renamed plotWaveformsLeCroy.m to plotStackedWaveforms.m

---
+++ plotStackedWaveforms.m
@@ -0,0 +1,46 @@
+function [  ] = plotStackedWaveforms( data, filename )
+% PLOTWAVEFORMS Plots the waveforms given in the columns of DATA on top of
+% each other. Before plotting, the mean value of each waveform is
+% substracted from it.
+%   
+%   PLOTWAVEFORMS(data) Plots the waveforms on top of each other.
+%   
+%   PLOTWAVEFORMS(data,filename) Plots the waveforms in data and outputs
+%   the resulting graph to FILENAME. Does the filename include an
+%   extenstion, this will be used as the filetype. If not, 'png' is the
+%   standard filetype.
+
+assert(ischar(filename),'FILENAME is not a character array!');
+assert(ismatrix(data),'DATA is not a matrix!');
+[rows, columns] = size(data);
+
+for column=1:columns
+    y = data(:,column);
+    y = y - mean(y);
+    plot(y,'b');
+    hold on;
+end
+
+xlabel('time');
+ylabel('voltage');
+
+switch nargin
+    case 2
+        [~,~,extension]=fileparts(filename);
+        if not(isempty(extension))
+            extension = strsplit(extension,'.');
+            filetype = char(extension(2));
+        else
+            filetype = 'png';
+        end
+        
+        if strcmp(filetype,'jpg')
+            filetype = 'jpeg';
+        end
+        filetype = ['-d' filetype];
+        
+        assert(ischar(filetype),'FILETYPE is not a character array!');
+        print(filename,filetype);
+end
+
+end
