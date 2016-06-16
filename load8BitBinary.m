bec70d2d895931144d7e8626e8836d9abeb239c5
lab <lab@SCORE-HD.e2.physik>
2016-06-16 14:55:56 +0200

    Modified: load8BitBinary

--- load8BitBinary.m
+++ load8BitBinary.m
@@ -4,14 +4,33 @@ function [ data8bit, config, timestamps ] = load8BitBinary( filename )
 %
 %   DATA8BIT = LOAD8BITBINARY(filename) Returns a 2D-Array...
 
+% Open the raw-data file, the configuration file and the timestamps file
 assert(exist(filename,'file')==2,['The given file "' filename '" does not exist!']);
 datafileID = fopen(filename);
+data8bit = [];
 
 config_filename = [filename '.cfg'];
 assert(exist(config_filename,'file')==2,['There is no *.cfg-file ' config_filename '!']);
-configfileID = fopen(config_filename);
+config = cfg2struct(config_filename);
 
+timestamps_filename = [filename '.stamp'];
+timestamps = [];
+if exist(timestamps_filename,'file')==2
+    timestampsfileID = fopen(timestamps_filename);
+    exist_timestamps = true;
+else
+    disp('Warning: No timestamps file detected!');
+    exist_timestamps = false;
+end
+
+% Determine number of channels
+% config.SpectrumCard.Channel01.Enable
+
+% Close all open files
 fclose(datafileID);
-fclose(configfileID);
+if exist_timestamps
+    fclose(timestampsfileID);
+end
+
 end
 
