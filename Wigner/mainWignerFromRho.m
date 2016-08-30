915677ae11380792111d6fd2f21e4ac0c53fb152
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2016-08-30 12:02:35 +0200

    Small updates to mainWignerFromRho.m

---
+++ Wigner/mainWignerFromRho.m
@@ -0,0 +1,14 @@
+function WF = mainWignerFromRho(rho)
+
+directory='D:\@archive\2016-08-30-wigner-test'
+
+maxn=size(rho,1);
+load(strcat(directory,'\n0m0'));
+WF=zeros(size(WigTab,1),size(WigTab,2));
+for nVal=0:maxn-1
+    for mVal=0:maxn-1
+        loadstring=strcat(directory,'\n',int2str(nVal),'m',int2str(mVal));
+        load (loadstring);
+        WF=WF+(rho(nVal+1,mVal+1)*WigTab);
+    end;
+end
