c0c291da343105db538d9ea09c2e6b6f466fc730
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2016-09-08 11:54:44 +0200

    'plotPowerSpectra' will create a plot of the power spectra from a detector standard test

---
+++ plotPowerSpectra.m
@@ -0,0 +1,9 @@
+function [  ] = plotPowerSpectra(  )
+%PLOTPOWERSPECTRA Summary of this function goes here
+%   Detailed explanation goes here
+%   Assumes the datafiles in the directory 'raw-data'.
+
+
+
+end
+
