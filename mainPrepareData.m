988160c5872152674869fe0accb116be40508342
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2016-11-10 15:39:53 +0100

    Small improvements.

--- mainPrepareData.m
+++ mainPrepareData.m
@@ -3,12 +3,16 @@ function [ X, theta ] = mainPrepareData( filename )
 %   Detailed explanation goes here
 
 WINDOWSIZE = 40;
+CALIBRATION_CH1 = 3.462042300029793e+03;
+NLO = 4.379815002805815e+08;
 
 [data8bit,config,timestamps]=load8BitBinary(filename,'dontsave');
 theta = piezoCalibration(data8bit, timestamps, config);
 [locs,~] = pointwiseVariance(data8bit);
 [~, X] = correlation(0, data8bit, locs, WINDOWSIZE);
 X = piezoSegments(timestamps, X);
+X = X * CALIBRATION_CH1;
+X = X / sqrt(2 * NLO);
 
 % Saving important Variables (to delete the raw data manually)
 dateString = datestr(datetime('now'),'yyyymmddTHHMMSS');
@@ -16,4 +20,3 @@ save(['quadratureDataset-' dateString '.mat'], ...
     'X', 'theta');
 
 end
-
