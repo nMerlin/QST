f156618c1ec932df8ff803303b089029d44ec54e
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2016-11-11 10:59:15 +0100

    Included 'plotWigner' to create pictures of the Wigner function

---
+++ Wigner/plotWigner.m
@@ -0,0 +1,26 @@
+function [  ] = plotWigner( WF, varargin )
+%PLOTWIGNER Creates a plot of the given Wigner Function
+%
+%   Noes:
+%   p and q have to be set manually
+
+p = -20:0.125:20;
+q = -20:0.125:20;
+
+image = 0;
+surface = 0;
+if nargin > 1
+    for i = 2:nargin
+        eval([varargin{i-1} '=1;']);
+    end
+end
+
+if image == 1
+    imagesc(p,q,real(WF));
+elseif surface == 1
+    h = surf(p,q,real(WF));
+    set(h,'LineStyle','none');
+end
+
+end
+
