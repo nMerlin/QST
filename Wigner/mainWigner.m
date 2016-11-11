f4ade7f47fef1a306e917b750b4bb83ad3dd7931
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2016-11-11 10:14:36 +0100

    Added 'overwrite' option to not overwrite existing matrix elements in the default case

--- Wigner/mainWigner.m
+++ Wigner/mainWigner.m
@@ -2,6 +2,10 @@ function mainWigner( nMax, mMax, varargin )
 %MAINWIGNER Calculates FT(<q+1/2*q'|n>*<m|q-1/2*q'>) up to n=NMAX and m =
 %MMAX. Set the discretization parameters and target directory in the source
 %code.
+%
+% Options:
+% 'parallel': Use MatLabs parallel pool for higher speed
+% 'overwrite': Compute existing matrix element again
 
 %directory = 'Z:\freefilesync-lab\matlab\QST\Wigner';
 %directory = 'D:\@archive\2016-08-30-wigner-test';
@@ -9,8 +13,9 @@ directory = 'C:\Users\lab\Documents\@data\Wigner';
 minq=-20;
 maxq=20;
 qintstep=0.125; %choose 1/2^n to avoid artifacts
-parallel = 0;
 
+parallel = 0;
+overwrite = 0;
 if nargin > 2
     for i = 3:nargin
         eval([varargin{i-2} '=1;']);
@@ -24,7 +29,7 @@ if parallel==1
     parfor i=0:(nMax+1)*(mMax+1)-1
         nVal = floor(i/(mMax+1));
         mVal = mod(i,mMax+1);
-        calcWignerTable(nVal,mVal,directory,minq,maxq,qintstep,'parallel');
+        calcWignerTable(nVal,mVal,directory,minq,maxq,qintstep,overwrite);
         dispstat('','init');
         dispstat(strcat('FT(<q+1/2*q''|',int2str(nVal),'>*<', ...
             int2str(mVal),'|q-1/2*q''>)',' computed!'), ...
@@ -37,7 +42,7 @@ else
         dispstat(strcat('Computing',' FT(<q+1/2*q''|', ...
             int2str(nVal),'>*<',int2str(mVal),'|q-1/2*q''>)','...'), ...
             'timestamp','keepthis');
-        calcWignerTable(nVal,mVal,directory,minq,maxq,qintstep);
+        calcWignerTable(nVal,mVal,directory,minq,maxq,qintstep,overwrite);
     end
 end
 
