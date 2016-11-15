e6d9c12ecd528d98c48f2ffb19ed1cf0c708821f
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2016-11-15 11:42:39 +0100

    adjusted to the modified "correlation" function

--- mainPrepareData.m
+++ mainPrepareData.m
@@ -2,14 +2,13 @@ function [ X, theta ] = mainPrepareData( filename )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-WINDOWSIZE = 40;
 CALIBRATION_CH1 = 3.462042300029793e+03;
 NLO = 4.379815002805815e+08;
 
 [data8bit,config,timestamps]=load8BitBinary(filename,'dontsave');
 theta = piezoCalibration(data8bit, timestamps, config);
 [locs,~] = pointwiseVariance(data8bit);
-[~, X] = correlation(0, data8bit, locs, WINDOWSIZE);
+[~, X] = correlation(0, data8bit, locs);
 X = piezoSegments(timestamps, X);
 X = X * CALIBRATION_CH1;
 X = X / sqrt(2 * NLO);
