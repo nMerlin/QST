d10f13040f7a1df5c9e0d7a4c6550511af79ee2c
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2016-11-17 10:46:50 +0100

    Removal of unphysical offsets in quadratures implemented.

--- mainPrepareData.m
+++ mainPrepareData.m
@@ -6,12 +6,22 @@ CALIBRATION_CH1 = 3.462042300029793e+03;
 NLO = 4.379815002805815e+08;
 
 [data8bit,config,timestamps]=load8BitBinary(filename,'dontsave');
-theta = piezoCalibration(data8bit, timestamps, config);
 [locs,~] = pointwiseVariance(data8bit);
 [~, X] = correlation(0, data8bit, locs);
 X = piezoSegments(timestamps, X);
+
+% Calibration of quadratures
 X = X * CALIBRATION_CH1;
 X = X / sqrt(2 * NLO);
+
+% Compute relative phases
+[X, theta] = computeTheta(X);
+
+% Remove unphysical offset
+for iSeg = 1 : size(X,2)
+    Seg = X(:,iSeg);
+    X(:,iSeg) = Seg - (max(Seg) - 0.5 * (max(Seg) - min(Seg)));
+end
 
 % Saving important Variables (to delete the raw data manually)
 dateString = datestr(datetime('now'),'yyyymmddTHHMMSS');
