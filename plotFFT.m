a8affcfbcca9c76d38d9b3491955030505bfbe98
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-05-12 10:28:19 +0200

    plotFFT plots a fourier spectrum of data in the given file

--- plotFFT.m
+++ plotFFT.m
@@ -1,8 +1,15 @@
-function [  ] = plotFFT( signal, samplerate )
+function [ locs ] = plotFFT( filename )
 %PLOTFFT taken from https://de.mathworks.com/help/matlab/ref/fft.html
 %   SIGNAL: Recorded Spectraum
 %   SAMPLERATE: Sampling Frequency
 
+% Loading
+[data8bit,config,~]=load8BitBinary(filename,'dontsave');
+originalSamplerate = config.SpectrumCard.Clock.SamplingRate0x28MHz0x29_DBL;
+signal = mean(data8bit(:,:,1));
+samplerate = round(originalSamplerate*1e6/size(data8bit,1));
+
+% Calculating
 T = 1/samplerate;
 L = length(signal);
 Y = fft(signal);
@@ -11,10 +18,17 @@ P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f = samplerate*(0:(L/2))/L;
 
-plot(f,P1);
+% Plotting
+[pks,locs] = findpeaks(P1,f,'MinPeakProminence',max(P1)/10);
+findpeaks(P1,f,'MinPeakProminence',max(P1)/10);
+text(locs+10,pks,num2str(locs'));
 title('Single-Sided Amplitude Spectrum');
 xlabel('f (Hz)');
 ylabel('|P1(f)|');
-axis([0 500 0 5]);
-end
+axis([0 500 0 max(P1)]);
 
+% Saving 
+outputFilename = ['FFT-' filename '.jpg'];
+outputFiletype = '-djpeg';
+print(outputFilename,outputFiletype);
+end
