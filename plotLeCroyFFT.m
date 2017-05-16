acc9dd2ef86bcd0b3f024607a16ce84dc54acd76
CarolinLueders <carolin.lueders@hotmail.de>
2017-05-16 09:27:12 +0200

    use findpeaks to plot

--- plotLeCroyFFT.m
+++ plotLeCroyFFT.m
@@ -10,18 +10,15 @@ Npeaks = 3;
 
 % Plotting
 [pks,locs] = findpeaks(fft,f,'MinPeakProminence',max(abs(fft))/4, 'Npeaks',Npeaks);  %,'MinPeakProminence',max(fft)/10
-
-plot(f,fft);
-hold on;
-plot(locs,pks+2,'v','markerFaceColor','b','markerEdgeColor','b');
+findpeaks(fft,f,'MinPeakProminence',max(abs(fft))/4, 'Npeaks',Npeaks);
 for i = 1:min(Npeaks,length(locs))
-    text(locs(i)+10,pks(i),num2str(locs(i)));
+   text(locs(i)+10,pks(i),num2str(locs(i)));
 end
 title('Single-Sided Amplitude Spectrum from LeCroy');
 xlabel('f (Hz)');
 ylabel('Spectral Power (dB)');
 axis([0 500 min(fft) 0]);
-grid();
+
 
 % Saving 
 outputFilename = ['LeCroy' filename '.jpg'];
