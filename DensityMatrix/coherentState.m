5380f96d666b54d3d893553d5f20cdb1d5bc2eee
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-06-06 12:40:09 +0200

    COHERENTSTATE now returns the density matrix of a coherent state

--- DensityMatrix/coherentState.m
+++ DensityMatrix/coherentState.m
@@ -1,21 +1,17 @@
-function [ rho ] = coherentState( nMax, Ns )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+function [ rho ] = coherentState( nMax, nAv )
+%COHERENTSTATE Coherent state density matrix in Fock basis
+%   Paramters:
+%   NMAX - Size of the density matrix minus 1
+%   NAV - Average photon number of the coherent state
 
 % Coherent state density matrix in the Fock (number) state representation
-rho =zeros(nMax+1,1);
-% In this program, all matrices are diagonal
-if (Ns <100)
-    for I=1:(nMax+1)
-        rho(I) =exp(-Ns) *Ns^(I-1) /factorial(I-1);
-    end
-else% Use Stirling's approximation
-    for I=1:(nMax+1)
-        rho(I) =exp(I-1-Ns) *(Ns/(I-1))^(I-1) /sqrt(2*pi*(I-1));
+alpha = sqrt(nAv);
+rho = zeros(nMax+1,1);
+for n = 1:(nMax+1)
+    for m = 1:(nMax+1)
+        rho(n,m) = alpha^(n+m)/sqrt(factorial(n)*factorial(m))*...
+            exp(-abs(alpha)^2);
     end
 end
 
-rho = diag(rho);
-
 end
-
