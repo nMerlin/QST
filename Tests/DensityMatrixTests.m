429bd7c32db936e1bcb4360d05fb453df34145e6
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-06-06 17:35:49 +0200

    Included normalization test for thermalState in DensityMatrixTests.m

--- Tests/DensityMatrixTests.m
+++ Tests/DensityMatrixTests.m
@@ -1,4 +1,4 @@
-function tests = coherentStateTest
+function tests = DensityMatrixTests
 % COHERENTSTATETEST Verifies the correct behavior of COHERENTSTATE
 tests = functiontests(localfunctions);
 end
@@ -9,6 +9,17 @@ expSolution = 1;
 
 % Fetch actual trace
 rho = coherentState(30,5);
+actSolution = trace(rho);
+
+verifyEqual(testCase,actSolution,expSolution,'AbsTol',100*eps);
+end
+
+function testThermalStateNorm(testCase)
+% Trace of density matrix should be 1
+expSolution = 1;
+
+% Fetch actual trace
+rho = thermalState(30,5);
 actSolution = trace(rho);
 
 verifyEqual(testCase,actSolution,expSolution,'AbsTol',100*eps);
