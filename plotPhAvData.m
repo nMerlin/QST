f1c66c45020ee243b521634b1e2c8b8380a48a7c
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-06-08 16:50:10 +0200

    plotPhAvData.m nicer plot

--- plotPhAvData.m
+++ plotPhAvData.m
@@ -7,25 +7,42 @@ X = X(:);
 X = X - mean(mean(X));
 
 % Mean photon number
-nAv = 0.5*(2*mean(X.^2))-0.5;
+nAv = mean(X.^2)-0.5;
 
-% Simulate theoretical thermal state with nAv photons
+% Simulations only if nMax<101
 nMax = max(ceil(3*nAv),30);
-rho = thermalState(nMax,nAv);
-WF = real(mainWignerFromRho(rho))/64;
-qThermal = sum(WF);
+if nMax<101
+    % Simulate theoretical thermal state with nAv photons
+    assert(nMax<101,'nMax too high for simulation');
+    rho = thermalState(nMax,nAv);
+    WF = real(mainWignerFromRho(rho))/64;
+    qThermal = sum(WF);
 
-% Simulate theoretical coherent state with nAv photons
-rho = coherentState(nMax,nAv);
-WF = real(mainWignerFromRho(rho))/64;
-qCoherent = phaseAveragedDist(WF);
+    % Simulate theoretical coherent state with nAv photons
+    rho = coherentState(nMax,nAv);
+    WF = real(mainWignerFromRho(rho))/64;
+    qCoherent = phaseAveragedDist(WF);
+end
 
 % Plot results
 edges = -20.0625:0.125:20.0625;
-histogram(X,edges,'Normalization','probability');
-hold on;
-plot(-20:0.125:20,qThermal,'linewidth',2);
-plot(-20:0.125:20,qCoherent,'linewidth',2);
+xAxis = -20:0.125:20;
+h = histogram(X,edges,'Normalization','probability');
+if nMax<101
+    hold on;
+    plot(xAxis,qThermal,'linewidth',2);
+    plot(xAxis,qCoherent,'linewidth',2);
+    legend('Measured','Thermal','Coherent');
+else
+    legend('Measured');
+end
+xlabel('Q');
+ylabel('Probability');
+title(['Phase-averaged quantum state (n=',num2str(nAv),')']);
+% Adjust axis limits
+minX = -2*fwhm(xAxis,h.Values);
+xlim([minX -minX]);
+hold off;
 
 end
 
