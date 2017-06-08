51b87fb8a1acfc98650e13ca1242ef55df49cd87
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-06-08 17:30:37 +0200

    plotPhAvData.m now saves the resulting plot

--- plotPhAvData.m
+++ plotPhAvData.m
@@ -1,4 +1,4 @@
-function [ nAv ] = plotPhAvData( X )
+function [ nAv ] = plotPhAvData( X, filename )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -39,10 +39,14 @@ end
 xlabel('Q');
 ylabel('Probability');
 title(['Phase-averaged quantum state (n=',num2str(nAv),')']);
+
 % Adjust axis limits
 minX = -2*fwhm(xAxis,h.Values);
 xlim([minX -minX]);
 hold off;
+
+% Save figure
+print(strcat('PHAV-',filename,'-',num2str(nAv),'photons', '-dpng');
 
 end
 
