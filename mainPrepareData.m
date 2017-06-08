826515c43251e6c07b18274b3e071cb6966d747b
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-06-08 12:43:55 +0200

    Cosmetic changes on mainPrepareData.m

--- mainPrepareData.m
+++ mainPrepareData.m
@@ -1,12 +1,13 @@
 function [ X, theta ] = mainPrepareData( filenameLO, filenameSIG)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-Norm = 1/sqrt(2);
-%Norm ist the factor in the relation between the quadratures and the ladder
-%operators: q = Norm*(a^{+} + a), p = Norm*i*(a^{+} - a)
-%typical values are 1/sqrt(2) or 1/2. 
 
 CALIBRATION_CH1 = 4.596047840078126e-05; % Ampere per Volt
+
+%Norm ist the factor in the relation between the quadratures and the ladder
+%operators: q = Norm*(a^{+} + a), p = Norm*i*(a^{+} - a)
+%typical values are 1/sqrt(2) or 1/2.
+Norm = 1/sqrt(2);
 
 % Compute number of LO photons
 dispstat('Computing number of LO photons ...','timestamp','keepthis',0);
@@ -31,7 +32,7 @@ X = Norm * X / sqrt(NLO);
 % Saving important Variables (to delete the raw data manually)
 dispstat('Saving ...','timestamp','keepthis',0);
 dateString = datestr(datetime('now'),'yyyymmddTHHMMSS');
-save(['quadratureDataset-' strrep(num2str(filenameSIG),'LOwithLO.raw','') dateString '.mat'], ...
-    'X', 'theta');
+save(['quadratureDataset-', strrep(num2str(filenameSIG),...
+    'LOwithLO.raw','') dateString '.mat'], 'X', 'theta');
 
 end
