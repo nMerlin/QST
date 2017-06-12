6bbc8003bfe17c9ba41c20a2cb91b7d421f2b290
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-06-12 15:09:20 +0200

    plotPhAvData.m bin-width equals minimum distance of quadratures which are naturally discretized

--- Plot/plotPhAvData.m
+++ Plot/plotPhAvData.m
@@ -29,12 +29,24 @@ if nMax<101
 end
 
 % Plot results
-edges = -30.0625:0.125:30.0625;
-xHist = -30:0.125:30;
+% edges = -30.0625:0.125:30.0625;
+% xHist = -30:0.125:30;
 xAxis = -20:0.125:20;
-h = histogram(X,edges,'Normalization','probability');
+
+% Adjust histogram edges to discretization and plot histogram
+uniq = unique(X(:));
+hDisc = min(diff(uniq)); % discretization
+histEdges = (min(uniq)-hDisc/2):hDisc:(max(uniq)+hDisc/2);
+xHist = min(uniq):hDisc:max(uniq);
+h = histogram(X,histEdges,'Normalization','probability');
+h.EdgeColor = 'b';
+
+% Plot theory curves
 if nMax<101
     hold on;
+    renorm = min(diff(xAxis))/hDisc;
+    qThermal = qThermal/renorm;
+    qCoherent = qCoherent/renorm;
     plot(xAxis,qThermal,'linewidth',2);
     plot(xAxis,qCoherent,'linewidth',2);
     legend('Measured','Thermal','Coherent');
@@ -42,8 +54,10 @@ if nMax<101
     % Calculate euclidean distance from both theoretical states
     start = (length(h.Values) - length(qThermal))/2+1;
     stop = start + length(qThermal) - 1;
-    distTherm = sqrt(sum((qThermal - h.Values(start:stop)).^2));
-    distCoh = sqrt(sum((qCoherent - h.Values(start:stop)).^2));
+%     distTherm = sqrt(sum((qThermal - h.Values(start:stop)).^2));
+%     distCoh = sqrt(sum((qCoherent - h.Values(start:stop)).^2));
+    distTherm = [];
+    distCoh = [];
 else
     legend('Measured');
     [distTherm,distCoh] = deal(NaN);
@@ -54,8 +68,6 @@ title({filename,['Phase-averaged quantum state (n=',num2str(nAv),')']});
 
 % Adjust axis limits
 width = fwhm(xHist,h.Values);
-% minX = -2*width;
-% xlim([minX -minX]);
 maxX = 2*sqrt(nAv)+3;
 xlim([-maxX maxX]);
 hold off;
