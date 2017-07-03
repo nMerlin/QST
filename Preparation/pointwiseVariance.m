cee1379b26bd2abe982a38f733e09f4685550b3b
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-07-03 12:02:39 +0200

    pointwiseVariance.m is now able to plot the found peaks (no ambiguity between plotting and finding any more...)

--- Preparation/pointwiseVariance.m
+++ Preparation/pointwiseVariance.m
@@ -1,4 +1,4 @@
-function [ locs, pvar ] = pointwiseVariance( data )
+function [ locs, pvar ] = pointwiseVariance( data, varargin )
 % POINTWISEVARIANCE  Calculates the pointwise variance of the given data
 % arrays. The input DATA is a matrix and each column is treated as a
 % separate data array. The algorithm removes the dc-offset before the
@@ -9,9 +9,25 @@ function [ locs, pvar ] = pointwiseVariance( data )
 %
 %   [LOCS,PVAR] = POINTWISEVARIANCE(DATA) Additionally outputs the array
 %   PVAR which consists of the pointwise variance data points.
+%
+%   [___] = POINTWISEVARIANCE(___,'showplot') additionally plots the signal
+%   and overlays the peak values
 
+%% Handle optional input arguments
+verbose = 0;
+showplot = 0;
+quiet = 'notquiet';
+if nargin > 1
+    for i = 1:nargin-1
+        eval([varargin{i} '=1;']);
+    end
+end
+if verbose == 0
+    quiet = 'quiet';
+end
+
+%% Subtract DC offset and calculate pointwise variance
 assert(ismatrix(data),'DATA is not a matrix!');
-
 [rows,columns] = size(data);
 pvar = zeros(rows,1);
 
@@ -23,7 +39,13 @@ for row=1:rows
     pvar(row) = var(double(data(row,:)));
 end
 
-[~,locs] = findpeaks(pvar,'MinPeakHeight',max(pvar)*0.15,'MinPeakDistance',40);
+%% Find peaks
+options.MinPeakHeight = mean(pvar);
+options.MinPeakDistance = 10;
+[~,locs] = findpeaks(pvar,options);
+if showplot == 1
+    findpeaks(pvar,options);
+end
 assert(not(isempty(locs)),'No maxima found in the pointwise variance!');
 
 end
