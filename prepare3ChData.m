63086f03b7fcfe7f3a9b723c44649dff56b21f74
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-07-04 09:51:26 +0200

    'prepare3ChData.m': Cosmetic changes and 'timestamps' as additional output argument.

--- prepare3ChData.m
+++ prepare3ChData.m
@@ -1,4 +1,4 @@
-function [ X1, X2, X3 ] = prepare3ChData( filenameLO, filenameSIG )
+function [X1, X2, X3, timestamps] = prepare3ChData(filenameLO, filenameSIG)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -13,21 +13,25 @@ dispstat('Load LO data','timestamp','keepthis',0);
 [data8bitLO,configLO,~]=load8BitBinary(filenameLO,'dontsave');
 
 dispstat('Load Signal data','timestamp','keepthis',0);
-[data8bitSIG,configSIG,~]=load8BitBinary(filenameSIG,'dontsave');
+[data8bitSIG,configSIG,timestamps]=load8BitBinary(filenameSIG,'dontsave');
 
 
 for iCh = 1:3
     % Compute number of LO photons
-    dispstat(['Computing number of LO photons for Channel ' num2str(iCh)],'timestamp','keepthis',0);
-    XLO = computeQuadratures(data8bitLO(:,:,iCh), configLO, CALIBRATION_CH1);
+    dispstat(['Computing number of LO photons for Channel ' ...
+        num2str(iCh)],'timestamp','keepthis',0);
+    XLO = computeQuadratures(data8bitLO(:,:,iCh), configLO, ...
+        CALIBRATION_CH1);
 
-    % Calculate the variance piece-wise to compensate slow drifts (e.g. piezos)
+    % Calculate the variance piece-wise to compensate slow drifts (e.g.
+    % piezos)
     NLO = mean(var(XLO));
 
     % Compute quadratures for target quantum state
-    dispstat(['Computing quadratures for target quantum state for Channel ' num2str(iCh)],...
-      'timestamp','keepthis',0);
-    X = computeQuadratures(data8bitSIG(:,:,iCh), configSIG, CALIBRATION_CH1);
+    dispstat(['Computing quadratures for target quantum state ' ...
+        'for Channel ' num2str(iCh)],'timestamp','keepthis',0);
+    X = computeQuadratures(data8bitSIG(:,:,iCh), configSIG, ...
+        CALIBRATION_CH1);
 
     % Calibration of quadratures to vacuum state
     X = Norm * X / sqrt(NLO);
@@ -41,8 +45,6 @@ for iCh = 1:3
             X3 = X;
     end
 end
-
-
 
 end
 
