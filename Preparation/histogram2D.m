f01913ec278054a698538453d067012c229fbe6e
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-07-10 15:56:13 +0200

    'histogram2D.m': New function to compute and plot 2D-histograms

---
+++ Preparation/histogram2D.m
@@ -0,0 +1,44 @@
+function [H, binsA, binsB] = histogram2D(A,B,varargin)
+%HISTOGRAM2D Compute the 2D histogram of the data given in A and B
+%
+% Optional input arguments:
+%   histogram2D(A,B,'plot'): Compute the histogram and plot its heat map.
+%       Default value is 'noplot'.
+%   histogram2D(A,B,~,binsA,binsB): Have exactly binsA bins in the
+%       A-direction and binsB bins in the B-direction. Default value is 0
+%       and will result in as many bins as are necessary to retain existing
+%       discretization (only useful for previously discretized data).
+
+%% Handle optional input arguments
+nVarargin = length(varargin);
+optArgs = {'noplot' 0 0};
+optArgs(1:nVarargin) = varargin;
+[plotArg,nBinsA,nBinsB] = optArgs{:};
+
+%% Calculate binning and histogram
+if nBinsA == 0
+    [binsA, nBinsA] = minBins(A);
+else
+    binsA = linspace(min(A),max(A),nBinsA);
+end
+if nBinsB == 0
+    [binsB, nBinsB] = minBins(B);
+else
+    binsB = linspace(min(B),max(B),nBinsB);
+end
+% map A and B to bin indices
+Ai = round(interp1(binsA,1:nBinsA,A,'linear','extrap'));
+Bi = round(interp1(binsB,1:nBinsB,B,'linear','extrap'));
+% limit indices to [1,nBins]
+Ai = max(min(Ai,nBinsA),1);
+Bi = max(min(Bi,nBinsB),1);
+% Count number of elements in each bin
+H = accumarray([Ai(:) Bi(:)], 1, [nBinsA nBinsB]);
+
+%% Plot
+if strcmp(plotArg,'plot')
+    imagesc(binsA,binsB,H), axis on
+    colormap hot; colorbar;
+end
+
+end
