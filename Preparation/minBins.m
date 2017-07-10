0fe5c3ec301bcd29d8c97f586e810a0cf7cffd9e
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-07-10 15:55:34 +0200

    'minBins.m': Updated description.

--- Preparation/minBins.m
+++ Preparation/minBins.m
@@ -2,12 +2,11 @@ function [bins, nBins] = minBins(A)
 %MINBINS Compute bin centers for minimum bin distance in A
 %
 % Note: The input array A should already exhibit some sort of
-% discretization
+% coarse discretization.
 
 uniq = unique(A(:));
-hDisc = min(diff(uniq));
+hDisc = min(diff(uniq(diff(uniq)>1000*eps)));
 bins = min(uniq):hDisc:max(uniq); % bin centers
 nBins = numel(bins);
 
 end
-
