4ba7745d1fbb6717a96085fef347b89f3d15211c
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-07-18 11:39:15 +0200

    'computePhase.m': Fine tuned findpeaks parameters

--- Preparation/computePhase.m
+++ Preparation/computePhase.m
@@ -1,8 +1,12 @@
-function theta = computePhase(ys,piezoSign,varargin)
-%COMPUTEPHASE Reconstruct phase from smoothed cross-correlation ys
+function [theta, selSeg] = computePhase(ys,piezoSign,varargin)
+%COMPUTEPHASE Reconstruct phase from smoothed cross-correlation data
 %
 % Input Arguments:
 %   ys = smoothCrossCorr(Xa,Xb,varargin)
+%
+% Output Arguments:
+%   theta - Reconstructed phase values
+%   selSeg - the reconstruction was successfull for these segments
 
 %% Handle optional input arguments
 nVarargin = length(varargin);
@@ -10,21 +14,39 @@ optArgs = {'noplot'};
 optArgs(1:nVarargin) = varargin;
 [plotArg] = optArgs{:};
 
-%% Parameters for the function _findpeaks_
-peakOpts.MinPeakHeight = 0.3 * max(ys);
-peakOpts.MinPeakDistance = 0.3 * length(ys);
-peakOpts.MinPeakWidth = 0.01 * length(ys);
-
 [nPoints,nSegments] = size(ys);
 theta = zeros(nPoints,nSegments);
-
+selSeg = ones(1,nSegments);
 for iSeg = 1:nSegments
     y = ys(:,iSeg);
+    %% Parameters for the function _findpeaks_
+    % To reconstruct the phase, the algorithm has to identify maxima and
+    % minima in the data. This is done by the function _findpeaks_.
+    % However, the parameters have to be chosen carefully.
+    periodsPerSeg = 1.2; % Optional: Compute automatically from config.
+    peakOpts.MinPeakDistance = 0.6 * length(y)/periodsPerSeg;
+    % _MinPeakDistance_ is by far the most important parameter. It
+    % determines how far away of each other the found peaks must be. In our
+    % case, the data should exhibit a certain periodicity and we want to
+    % know where the maximum and minimum in each period is. Therefore, a
+    % _MinPeakDistance_ of roughly 50% of the period should do the trick.
+    peakOpts.MinPeakHeight = 0.5 * max(y);
+    % Because of different noise sources and instabilities, the distance
+    % between maxima and minima in _y_ is uncertain to some degree.
+    % _MinPeakHeight_ ensures, that only peaks above a certain threshold
+    % will be found. Because between two maxima there should be a minimum,
+    % _MinPeakHeight_ mitigates the effects of too long periods.
     
     %% Find peaks for flank recognition
     if strcmp(plotArg,'plot')
-        findpeaks(y,peakOpts); hold on;
-        findpeaks(-y,peakOpts); hold off;
+        findpeaks(y,peakOpts,'Annotate','extents'); hold on;
+        findpeaks(-y,peakOpts,'Annotate','extents'); hold off;
+        key = waitforbuttonpress;
+        if key == 0
+            theta(:,iSeg) = NaN(nPoints,1);
+            selSeg(iSeg) = false;
+            continue
+        end
     end
     [~,maxlocs] = findpeaks(y,peakOpts);
     [~,minlocs] = findpeaks(-y,peakOpts);
@@ -35,10 +57,10 @@ for iSeg = 1:nSegments
             num2str(iSeg),'!'));
     
     %% Sort peaks (assumption: we only see "global" maxima and minima)
-    [locs, I] = sort([maxlocs minlocs]);
+    [locs, I] = sort([maxlocs;minlocs]);
     nTurningPoints = length(locs);
     assert(nTurningPoints>1, 'Not enough turning points encountered!');
-    pks = [maxpks minpks];
+    pks = [maxpks;minpks];
     pks = pks(I);
     
     %% Look for extrema on left boundary
@@ -96,7 +118,7 @@ for iSeg = 1:nSegments
             normDiff = max(abs(pksDiff(1)),abs(leftex-pks(1)));
             maxValue = max([pks(1),pks(2),leftex]);
         elseif iPart == nTurningPoints
-            range = (locs(end)):length(smallTheta);
+            range = (locs(end)):length(theta(:,iSeg));
             normDiff = max(abs(pksDiff(end)),abs(rightex-pks(end)));
             maxValue = max([pks(end),pks(end-1),rightex]);
         else
@@ -106,8 +128,8 @@ for iSeg = 1:nSegments
         end
 
         % Scale y-Values to interval [-1;1] for asin
-        y = yFit(range);
-        ynorm = 2*(y-maxValue)/normDiff + 1;
+        ynorm = y(range);
+        ynorm = 2*(ynorm-maxValue)/normDiff + 1;
 
         % Correct for machine precision
         [~,iMax] = max(ynorm);
@@ -141,8 +163,15 @@ for iSeg = 1:nSegments
         s = s * (-1);
     end % iPart
     
+    if ~isreal(theta(1:end,iSeg))
+        theta(:,iSeg) = NaN(nPoints,1);
+        selSeg(iSeg) = false;
+        continue
+    end
     assert(isreal(theta),...
         ['Not all phase values are real in Segment ' num2str(iSeg) '.']);
 end % iSeg
+
+theta = mod(theta,2*pi);
 
 end % function
