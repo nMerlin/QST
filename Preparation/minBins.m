4ca582bfda805f019f134d44af1f126da0af95dd
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-07-20 10:58:06 +0200

    'minBins' has a new optional argument that lets you choose a binning that is symmetrical around 0.

--- Preparation/minBins.m
+++ Preparation/minBins.m
@@ -1,13 +1,28 @@
-function [bins, nBins, edges] = minBins(A)
+function [bins, nBins, edges] = minBins(A,varargin)
 %MINBINS Compute bin centers for minimum bin distance in A
 %
 % Note: The input array A should already exhibit some sort of
 % coarse discretization.
 
+%% Optional input paramters
+optArgs = {'asym'};
+optArgs(1:length(varargin)) = varargin;
+[symOpt] = optArgs{:};
+
 uniq = unique(A(:));
 hDisc = min(diff(uniq(diff(uniq)>1000*eps)));
-bins = min(uniq):hDisc:max(uniq); % bin centers
+
+%% Compute bin centers and edges
+% There are two options: The bins can either be calculated according to the
+% maximum and minimum of the data, or symmetrically around 0.
+if strcmp(symOpt,'asym')
+    bins = min(uniq):hDisc:max(uniq); % bin centers
+    edges = (min(uniq)-hDisc/2):hDisc:(max(uniq)+hDisc/2);
+elseif strcmp(symOpt,'sym')
+    maxVal = max(-min(uniq),max(uniq));
+    bins = -maxVal:hDisc:maxVal;
+    edges = (-maxVal-hDisc/2):hDisc:(maxVal+hDisc/2);
+end
 nBins = numel(bins);
-edges = (min(uniq)-hDisc/2):hDisc:(max(uniq)+hDisc/2);
 
 end
