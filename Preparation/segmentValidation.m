57d933cb2f5a062c049a2f90ca55627260ddd064
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-07-20 15:20:54 +0200

    'segmentValidation': Manual selection and saving implemented.

--- Preparation/segmentValidation.m
+++ Preparation/segmentValidation.m
@@ -15,5 +15,38 @@ defaultFilename = '';
 addParameter(p,'Filename',defaultFilename,@isstr);
 parse(p,varargin{:});
 
+%% Let the user choose the segments to reject
+% First, we need to calculate the smoothed cross correlation that was used
+% for phase reconstruction.
+ys = smoothCrossCorr(X1,X3);
+
+% Second, we loop over all piezo segments and ask the user whether he wants
+% to reject the segment or not
+[~,~,nSegments] = size(X1);
+segSel = zeros(nSegments,1);
+for iSeg = 1:nSegments
+    plot(ys(:,iSeg)); hold on;
+    plot(theta(:,iSeg)); hold off;
+    ylabel(['Segment ',num2str(iSeg)]);
+    title({'Mousebutton: Reject','Keyboard: Accept'});
+    key = waitforbuttonpress;
+    if key == 1 % keyboard
+        segSel(iSeg) = 1;
+    else % e.g. mouse
+        segSel(iSeg) = 0;
+    end
+end % iSeg
+
+% Third, apply the selection to the output parameters
+X1 = X1(:,:,segSel==1);
+X2 = X2(:,:,segSel==1);
+X3 = X3(:,:,segSel==1);
+theta = theta(:,segSel==1);
+
+%% Optional: Save the output paramters
+if ~isempty(p.Results.Filename)
+    save(['validatedSegments-',p.Results.Filename,'.mat'],'X1','X2','X3','theta');
+end
+
 end
 
