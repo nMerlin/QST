cdd108b71740a92ae93beb56ee4d32f3c0eba30a
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-07-20 10:21:50 +0200

    'minBins': Additionally outputs edges for histogram bins.

--- Preparation/minBins.m
+++ Preparation/minBins.m
@@ -1,4 +1,4 @@
-function [bins, nBins] = minBins(A)
+function [bins, nBins, edges] = minBins(A)
 %MINBINS Compute bin centers for minimum bin distance in A
 %
 % Note: The input array A should already exhibit some sort of
@@ -8,5 +8,6 @@ uniq = unique(A(:));
 hDisc = min(diff(uniq(diff(uniq)>1000*eps)));
 bins = min(uniq):hDisc:max(uniq); % bin centers
 nBins = numel(bins);
+edges = (min(uniq)-hDisc/2):hDisc:(max(uniq)+hDisc/2);
 
 end
