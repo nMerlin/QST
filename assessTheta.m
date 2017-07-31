2f8cb0e05f88e9fcb9f562abdd8128c896e61f7b
CarolinLueders <carolin.lueders@hotmail.de>
2017-07-31 18:01:38 +0200

    Better preallocation size of XOut.

--- assessTheta.m
+++ assessTheta.m
@@ -11,6 +11,10 @@ function [phaseVariance, varXvar ] = assessTheta(theta, X)
 %- phaseVariance: variance of the histogram values of the phase, i. e. of
 % the probability density.
 %- varXvar: The variance of the variance of the X values in each phase bin. 
+Norm = 1/sqrt(2);
+%Norm ist the factor in the relation between the quadratures and the ladder
+%operators: q = Norm*(a^{+} + a), p = Norm*i*(a^{+} - a)
+%typical values are 1/sqrt(2) or 1/2. 
 
 %% Create and plot histogram of phase 
 % The number of bins is 1000 by default.
@@ -41,7 +45,7 @@ nIntervals = 180;
 [~,I] = sort(bin);
 X = X(I);
 
-XOut = NaN(ceil(3*length(theta)/nIntervals), nIntervals);
+XOut = NaN(max(N), nIntervals);
 for iInterval = 1 : nIntervals
     start = 1+sum(N(1:iInterval-1));
     stop = start+N(iInterval)-1;
@@ -49,14 +53,17 @@ for iInterval = 1 : nIntervals
 end
 %compute mean and variance of Quadrature Values for each phase bin
 meanXBinned = mean(XOut, 'omitnan');
+meanXBinned2 = mean(XOut.^2, 'omitnan');
 varXBinned = var(XOut, 'omitnan');
 %Measure how constant the variance is
 varXvar = var(varXBinned, 'omitnan');
 
 waitforbuttonpress;
 clf();
-plot(1:nIntervals,meanXBinned,'b.',1:nIntervals,varXBinned,'r.');
-legend('Mean of phase-binned X','Variance of phase-binned X');
+plot(1:nIntervals,meanXBinned,'b.',1:nIntervals,varXBinned,'r.',1:nIntervals,meanXBinned2,'g.');
+hold on;
+plot(1:nIntervals,Norm^2*ones(nIntervals),'k-','lineWidth',0.5);
+legend('Mean of phase-binned X','Variance of phase-binned X','Variance for coherent state');
 xlabel('Number of Bin');
 text('Units','Normalized','Position',[0.1,0.1],'String',['Variance of Variance = ' num2str(varXvar)],'EdgeColor','k');
 title('Phase-Binned Quadrature Values');
