a1555b149a76e49827ca4f9632ffc0ed01b41c20
CarolinLueders <carolin.lueders@hotmail.de>
2017-08-04 17:15:13 +0200

    You can now choose between line and circle as selection region.

--- QSeries.m
+++ QSeries.m
@@ -1,41 +1,57 @@
-function QSeries(O1,O2,O3,oTheta)
+function QSeries(O1,O2,O3,oTheta,varargin)
+%QSERIES - 
+% Input arguments: Selected datapoints from a 3-Channel-Measurement where
+% the two channels O1 and O2 are orthogonal. (See in demo3ChannelAnalysis
+% how this is done.)
+%
+% Depending on 'Type', a region like a line or a fullcircle with a series of 
+% positions Q is postselected from the orthogonal channels phase space,
+% and the expectation values of the target channel are computed  and
+% plotted.
 
-Norm = 1/sqrt(2);
-%Norm ist the factor in the relation between the quadratures and the ladder
-%operators: q = Norm*(a^{+} + a), p = Norm*i*(a^{+} - a)
-%typical values are 1/sqrt(2) or 1/2. 
+%% Validate and parse input arguments
+p = inputParser;
+defaultType = 'Qline';
+addParameter(p,'Type',defaultType,@isstr);
+parse(p,varargin{:});
+c = struct2cell(p.Results);
+[type] = c{:};
 
-%make befor some steps e g select orthogonal
-%make here switch case for several region selections eg fullcircle
+%% Compute expectation values for every Q and plot them
 
-Q = linspace(0,6,13);
-[ expQmax,  expQ2max, delQmean, Unc, N]=deal(zeros(length(Q),1));
+Q = linspace(0,6,25);
+width = 0.5;
+[ expQmax,  expQ2max, expQ2min, delQmean, Unc, N]=deal(zeros(length(Q),1));
 
 for i = 1:length(Q)
-    [XQ,thetaQ] = selectRegion(O1,O2,O3,oTheta,'Type','Pline','Position',[Q(i) 0.5],'Plot','show');
+    [XQ,thetaQ] = selectRegion(O1,O2,O3,oTheta,'Type',type,'Position',...
+        [Q(i) width],'Plot','hide');        
     [XQdis,thetaQdis]=discretizeTheta(XQ,thetaQ,180);
     [ expQ, ~, expQ2, ~, delQ, ~, meanUnc,~ , meanN, ~] =...
-        computeExpectations2( XQdis, thetaQdis, ['11-Qline-' strrep(num2str(Q(i)),'.',',')] );
+        computeExpectations2( XQdis, thetaQdis,['11-' type '-Q-' ...
+        strrep(num2str(Q(i)),'.',',') '-w-' strrep(num2str(width),'.',',')] );
     expQmax(i) = max(expQ);
     expQ2max(i) = max(expQ2);
+    expQ2min(i) = min(expQ2);
     delQmean(i) = mean(delQ);
     Unc(i) = meanUnc;
     N(i) = meanN;
 end
 
+%% Plot expectation values over Q
 close all;
 hold off;
-plot(Q, expQmax, Q, expQ2max, Q, delQmean, Q, Unc, Q, N, 'linewidth', 2);
+plot(Q, expQmax, Q, expQ2max, Q, expQ2min, Q, delQmean, Q, Unc, Q, N, 'linewidth', 2);
 
 xlabel('Q_{PS}');
 %axis([0 max(Q) 0  ]);
 set(0,'DefaultLegendInterpreter','latex');
 set(0,'DefaultTextInterpreter','latex');
-legend('$<Q>_{max}$', '$<Q^{2}>_{max}$', '$<\Delta Q>$',...
+legend('$<Q>_{max}$', '$<Q^{2}>_{max}$', '$<Q^{2}>_{min}$', '$<\Delta Q>$',...
      '$<\Delta Q \cdot \Delta P>$ ', '$<n>$ ', 'location', 'best');
-title(strcat('11-Qline'));
+title(['11-' type '-Width-' num2str(width)]);
 
     % Save plot
-print('11-Qline-Qdependence', '-dpng');
+print(['11-' type '-w-' strrep(num2str(width),'.',',') '-Qdependence'], '-dpng');
 
 end
