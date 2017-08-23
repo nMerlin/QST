3f5861fd315927a4547a5404d01ae54239fba433
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-08-23 11:45:53 +0200

    'prepare3ChData': Now uses the more general 'computeQuadratures' that accounts for small timing errors and works with multi-channel 'data8bit'

--- Preparation/prepare3ChData.m
+++ Preparation/prepare3ChData.m
@@ -13,52 +13,51 @@ CALIBRATION_CH1 = 4.596047840078126e-05; % Ampere per Volt
 %typical values are 1/sqrt(2) or 1/2.
 Norm = 1/sqrt(2);
 
-dispstat('Load LO data','init','timestamp','keepthis',0);
+dispstat('','init','timestamp','keepthis',0);
+dispstat('Load LO data','timestamp','keepthis',0);
 [data8bitLO,configLO,~]=load8BitBinary(filenameLO,'dontsave');
 
 dispstat('Load Signal data','timestamp','keepthis',0);
 [data8bitSIG,configSIG,timestamps]=load8BitBinary(filenameSIG,'dontsave');
 
+% Compute number of LO photons
+dispstat('Computing number of LO photons for Channels 1 to 3.', ...
+    'timestamp','keepthis',0);
+XLO = computeQuadratures(data8bitLO(:,:,1:3), configLO, ...
+    CALIBRATION_CH1);
+
+% Calculate the variance piece-wise to compensate slow drifts (e.g.
+% piezos)
+NLO = mean(var(XLO));
+
+% Compute quadratures for target quantum state
+dispstat('Computing target quadratures for Channels 1 to 3', ...
+    'timestamp','keepthis',0);
+X = computeQuadratures(data8bitSIG(:,:,1:3), configSIG, ...
+    CALIBRATION_CH1);
 
 for iCh = 1:3
-    % Compute number of LO photons
-    dispstat(['Computing number of LO photons for Channel ' ...
-        num2str(iCh)],'timestamp','keepthis',0);
-    XLO = computeQuadratures(data8bitLO(:,:,iCh), configLO, ...
-        CALIBRATION_CH1);
-
-    % Calculate the variance piece-wise to compensate slow drifts (e.g.
-    % piezos)
-    NLO = mean(var(XLO));
-
-    % Compute quadratures for target quantum state
-    dispstat(['Computing quadratures for target quantum state ' ...
-        'for Channel ' num2str(iCh)],'timestamp','keepthis',0);
-    X = computeQuadratures(data8bitSIG(:,:,iCh), configSIG, ...
-        CALIBRATION_CH1);
-
     % Calibration of quadratures to vacuum state
-    X = Norm * X / sqrt(NLO);
+    X(:,:,iCh) = Norm * X(:,:,iCh) / sqrt(NLO(iCh));
     
     % Removing Offsets
-    dispstat('Removing piecewise offset ...','timestamp','keepthis',0);
-    X = bsxfun(@minus, X, mean(X));
+    dispstat(['Removing piecewise offset from channel ',num2str(iCh), ...
+        ' ...'],'timestamp','keepthis',0);
+    X(:,:,iCh) = bsxfun(@minus, X(:,:,iCh), mean(X(:,:,iCh)));
     
     % Cut the raw data into segments of equal length according to piezo
     % modulation
-    dispstat('Reshaping into piezo segments ...', ...
-        'timestamp','keepthis',0);
-    [X,piezoSign] = piezoSegments(timestamps,X,'cut');
-
+    dispstat(['Reshaping channel ',num2str(iCh), ...
+        ' into piezo segments ...'],'timestamp','keepthis',0);
+    [Y,piezoSign] = piezoSegments(timestamps,X(:,:,iCh),'cut');
     switch iCh
         case 1
-            X1 = X;
+            X1 = Y;
         case 2
-            X2 = X;
+            X2 = Y;
         case 3
-            X3 = X;
+            X3 = Y;
     end
-end
-
-end
+end % iCh
+end % prepare3ChData
 
