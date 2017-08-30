32f2820382ed768905ee184db8fb58b3255357dc
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-08-30 17:10:05 +0200

    'assessTheta': Improved plots, number of bins as input arguments

--- Helperfunctions/assessTheta.m
+++ Helperfunctions/assessTheta.m
@@ -1,4 +1,4 @@
-function [phaseVariance, varXvar ] = assessTheta(theta, X)
+function [phaseVariance, varXvar] = assessTheta(theta, X, varargin)
 %This function assesses the final result of X3 and theta. It plots the
 %distribution of the computed phase and the variance of the distribution.
 %It also sorts the X values into phase bins.
@@ -17,17 +17,32 @@ Norm = 1/sqrt(2);
 %typical values are 1/sqrt(2) or 1/2. 
 
 
+%% Validate and parse input arguments
+p = inputParser;
+% How many bins will be used to examine the distribution of phase values?
+defaultPhaseBins = 1000;
+% How many bins will be used to examine the variance in quadrature values?
+% If VarBins is too high, there are bins without or with only a few X
+% Values, which results in a high variance of the X variance.
+defaultVarBins = 180;
+addParameter(p,'PhaseBins',defaultPhaseBins,@isnumeric);
+addParameter(p,'VarBins',defaultVarBins,@isnumeric);
+parse(p,varargin{:});
+c = struct2cell(p.Results);
+[phaseBins,varBins] = c{:};
+
 %% Strip X and theta of their NaN values
 X = X(~isnan(X));
 theta = theta(~isnan(theta));
 
 %% Create and plot histogram of phase 
 % The number of bins is 1000 by default.
-xHist = linspace(0,2*pi,1000);
+xHist = linspace(0,2*pi,phaseBins);
 disc = min(diff(xHist));
 histEdges = (0-disc/2):disc:(2*pi+disc/2);
 
 h = histogram(theta,histEdges,'Normalization','pdf');
+% Normalization 'pdf' means 'Probability density function estimate'
 
 %variance of observations of phase per bin --> Shows if phase is equally 
 % distributed
@@ -36,39 +51,42 @@ phaseVariance = var(h.Values);
 h.EdgeColor = 'b';
 axis([0 2*pi 0 max(h.Values)+0.05]);
 xlabel('\theta');
-ylabel('probability density');
-title('Phase Distribution');
-text('Units','Normalized','Position',[0.6,0.9],'String',['Variance = ' num2str(phaseVariance)],'EdgeColor','k');
+ylabel('Estimation of Probability Density');
+title('Distribution of Phase Values');
+text('Units','Normalized','Position',[0.6,0.9],'String', ...
+    ['Variance = ' num2str(phaseVariance)],'EdgeColor','k');
 
 
 
-%% Sorting of Quadrature Values into Phase bins 
-nIntervals = 180; 
-% If nIntervals is too much, there are bins without or with
-% only few X Values, which result in a too high variance of the X variance.
-[N,~,bin] = histcounts(theta,nIntervals);
+%% Sorting of Quadrature Values into Phase Bins 
+[N,~,bin] = histcounts(theta,varBins);
 [~,I] = sort(bin);
 X = X(I);
 
-XOut = NaN(max(N), nIntervals);
-for iInterval = 1 : nIntervals
+XOut = NaN(max(N), varBins);
+for iInterval = 1 : varBins
     start = 1+sum(N(1:iInterval-1));
     stop = start+N(iInterval)-1;
     XOut(1:N(iInterval),iInterval) = X(start:stop);
 end
-%compute mean and variance of Quadrature Values for each phase bin
+% Compute mean and variance of Quadrature Values for each phase bin
 meanXBinned = mean(XOut, 'omitnan');
 varXBinned = var(XOut, 'omitnan');
-%Measure how constant the variance is
+% Measure how constant the variance is
 varXvar = var(varXBinned, 'omitnan');
 
-waitforbuttonpress;
-clf();
-plot(1:nIntervals,meanXBinned,'b.',1:nIntervals,varXBinned,'r.');
+%waitforbuttonpress;
+%clf();
+figure
+plot(1:varBins,meanXBinned,'b-',1:varBins,varXBinned,'r-');
 hold on;
-plot(1:nIntervals,Norm^2*ones(nIntervals),'k-','lineWidth',0.5);
-legend('Mean of phase-binned X','Variance of phase-binned X','Variance for coherent state');
+plot(1:varBins,Norm^2*ones(varBins),'k-','lineWidth',0.5);
+legend('Mean of phase-binned X','Variance of phase-binned X', ...
+    'Variance for coherent state');
 xlabel('Number of Bin');
-text('Units','Normalized','Position',[0.1,0.1],'String',['Variance of Variance = ' num2str(varXvar)],'EdgeColor','k');
+text('Units','Normalized','Position',[0.3,0.1],'String', ...
+    ['Variance of Variance = ' num2str(varXvar)],'EdgeColor','k');
 title('Phase-Binned Quadrature Values');
+set(gca,'YLim',[min(meanXBinned) max(varXBinned)+2]);
+
 end
