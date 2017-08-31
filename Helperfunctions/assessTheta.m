733e0c3c608da686faa93936cab1a58e512b3692
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-08-31 16:04:00 +0200

    assessTheta: Improved plots.

--- Helperfunctions/assessTheta.m
+++ Helperfunctions/assessTheta.m
@@ -20,11 +20,11 @@ Norm = 1/sqrt(2);
 %% Validate and parse input arguments
 p = inputParser;
 % How many bins will be used to examine the distribution of phase values?
-defaultPhaseBins = 180;
+defaultPhaseBins = 100;
 % How many bins will be used to examine the variance in quadrature values?
 % If VarBins is too high, there are bins without or with only a few X
 % Values, which results in a high variance of the X variance.
-defaultVarBins = defaultPhaseBins;
+defaultVarBins = 100;
 addParameter(p,'PhaseBins',defaultPhaseBins,@isnumeric);
 addParameter(p,'VarBins',defaultVarBins,@isnumeric);
 parse(p,varargin{:});
@@ -39,22 +39,25 @@ theta = theta(~isnan(theta));
 % The number of bins is 1000 by default.
 histEdges = linspace(0,2*pi,phaseBins+1);
 
-h = histogram(theta,histEdges,'Normalization','pdf');
+fig = figure('Units','centimeters','Position',[1,1,14.8,21]);
+ax1 = subplot(2,1,1);
+h = histogram(ax1,theta,histEdges); % 'Normalization','pdf');
 % Normalization 'pdf' means 'Probability density function estimate'
 
 %variance of observations of phase per bin --> Shows if phase is equally 
 % distributed
 phaseVariance = var(h.Values);
+Nbar = length(theta)/phaseBins;
+theoVar = sqrt(Nbar);
 
 h.EdgeColor = 'b';
 axis([0 2*pi 0 max(h.Values)+0.05]);
 xlabel('\theta');
-ylabel('Estimation of Probability Density');
-title('Distribution of Phase Values');
-text('Units','Normalized','Position',[0.6,0.9],'String', ...
-    ['Variance = ' num2str(phaseVariance)],'EdgeColor','k');
-
-
+ylabel('N(\theta)');
+title(['Distribution of Phase Values (',num2str(phaseBins),' Bins)']);
+legend(['mean(N(\theta)) = ',num2str(Nbar),'; Var(N(\theta))) = ',num2str(phaseVariance)]);
+%text('Units','Normalized','Position',[0.6,0.9],'String', ...
+%    ['Variance = ' num2str(phaseVariance)],'EdgeColor','k');
 
 %% Sorting of Quadrature Values into Phase Bins 
 [N,varEdges,bin] = histcounts(theta,varBins);
@@ -75,19 +78,22 @@ varXvar = var(varXBinned, 'omitnan');
 
 %waitforbuttonpress;
 %clf();
-figure
+%figure
+%hold on;
+ax2 = subplot(2,1,2);
 hold on;
 xAxis = varEdges(1:end-1)+min(diff(varEdges))/2;
-xHist = histEdges(1:end-1)+min(diff(histEdges))/2;
 plot(xAxis,meanXBinned,'b.',xAxis,varXBinned,'r-o');
 plot(xAxis,Norm^2*ones(varBins),'k.','lineWidth',0.5);
-plot(xHist,h.Values*10,'g-o');
-legend('Mean of phase-binned X','Variance of phase-binned X', ...
-    'Variance for coherent state');
+legend('Mean of phase-binned X values', ...
+    ['Var(X), Var(Var(X)) = ',num2str(varXvar)], ...
+    ['Var(Coherent State ) = ',num2str(Norm^2)]);
 xlabel('\theta');
-text('Units','Normalized','Position',[0.3,0.1],'String', ...
-    ['Variance of Variance = ' num2str(varXvar)],'EdgeColor','k');
-title('Phase-Binned Quadrature Values');
-set(gca,'YLim',[min(meanXBinned) max(varXBinned)+2]);
+title(['Phase-Binned Quadrature Values (',num2str(varBins),' Bins)']);
+set(gca,'YLim',[min(meanXBinned) max(varXBinned)+3],'XLim', ...
+    [min(xAxis) max(xAxis)]);
+fig.PaperPositionMode = 'auto';
+print(['Variance-',num2str(varBins),'-varBins-',num2str(phaseBins), ...
+    '-phaseBins','.pdf'],'-dpdf');
 
 end
