b4eafee9359c7527a145b44fd12dcfbd425e1213
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2017-09-01 11:37:18 +0200

    plotHusimi: Improved aspect ration and axis limits.

--- Plot/plotHusimi.m
+++ Plot/plotHusimi.m
@@ -3,5 +3,8 @@ function plotHusimi(O1,O2,iSelect)
     [H, binsO1, binsO2] = histogram2D(O1,O2);
     imagesc(binsO1,binsO2,H); axis on; colormap hot; hold on;
     plot(O1(iSelect),O2(iSelect),'.'); hold off;
+    set(gca,'XLim',[-7,7],'YLim',[-7,7]);
+    pbaspect([1 1 1]); % ensure that a circle doesn't look elliptic
+    title('Histogram of Husimi-Q and Selected Region');
 end
 
