4f8e5daa1d51daac98d4d8ff6dee92492fda4a3d
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-02-06 14:22:10 +0100

    cohCDF: Cumulative distribution function (and inverse) of integral projections from arbitrary coherent Wigner function.

---
+++ Wigner/cohCDF.m
@@ -0,0 +1,21 @@
+function out = cohCDF(in,theta,nAv,varargin)
+%COHCDF Cumulative distribution function of integral projections from
+%arbitrary coherent Wigner functions
+
+%% Validate and parse input arguments
+p = inputParser;
+defaultInverse = false;
+addParameter(p,'Inverse',defaultInverse,@islogical);
+parse(p,varargin{:});
+c = struct2cell(p.Results);
+[inverse] = c{:};
+
+%% Compute coherent cumulative distribution function or inverse
+if inverse
+    out = sqrt(2*nAv)*cosd(theta)+erfinv(-1+2*in);
+else
+    out = 1/2*(1 + erf(in-sqrt(2*nAv)*cosd(theta)));
+end
+
+end
+
