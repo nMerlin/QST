08afba03584abb19e6d7db40ee71b977e2c8842b
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-02-07 11:34:01 +0100

    simQST: Now correctly draws average photon numbers to model thermal state with coherent ones.

--- Simulations/simQST.m
+++ Simulations/simQST.m
@@ -22,31 +22,23 @@ if strcmp(type,'Thermal')
     Phases = rand(nSamples,1)*360;
 elseif strcmp(type,'Coherent')
     % Pre-defined phases (e.g. by piezo modulation)
-    Phases = mod(linspace(0,phaseperiods*360,nSamples),360);
+    Phases = mod(linspace(0,phaseperiods*360,nSamples),360)';
 end
 
-%% Step 2: Draw photon numbers
-PV = 0:0.1:5*nPhotons;
-probs = probThermPhotons(PV,nPhotons);
-probs = probs./sum(probs);
-photonCDF = cumsum(probs);
-nPhotonCDF = length(photonCDF);
+%% Step 2: Draw average photon numbers
 if strcmp(type,'Thermal')
-    % Draw Bose-Einstein distributed photon numbers and find their
-    % corresponding indices in PV
-    [~,iPhotons] = histc(rand(nSamples,1), ...
-        [-Inf interp1(1:nPhotonCDF,photonCDF,0.5+(1:nPhotonCDF-1)) Inf]);
-    Photons = PV(iPhotons);
+    % Draw average photon numbers from simple exponential distribution
+    r = rand(nSamples,1);
+    Photons = exponentialCDF(r,nPhotons,'Inverse',true);
 elseif strcmp(type,'Coherent')
-    [~,iPhotons] = histc(ones(nSamples,1)*nPhotons, ...
-        [-Inf interp1(1:nPV,PV,0.5+(1:nPV-1)) Inf]);
-    Photons = PV(iPhotons);
+    % Constant average photon number
+    Photons = ones(nSamples,1)*nPhotons;
 end
 
 %% Step 3: Draw quadratures
 % Draw uniformly distributed probabilities to get quadratures from CDF
 r = rand(nSamples,1);
-X = cohCDF(r,Phases,Photons','Inverse',true);
+X = cohCDF(r,Phases,Photons,'Inverse',true);
 
 end
 
