8ae95f4ae06c35febc6ba8a711461a1332459336
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-02-15 11:23:09 +0100

    plotCrossCorrelation: Added output parameters for amplitude of modulation.

--- Plot/plotCrossCorrelation.m
+++ Plot/plotCrossCorrelation.m
@@ -1,4 +1,4 @@
-function plotCrossCorrelation(X1, X2, X3, varargin)
+function [A12,A13,A23] = plotCrossCorrelation(X1, X2, X3, varargin)
 %PLOTCROSSCORRELATION Plot all three possible crosscorrelations
 %
 %   (X1,X2,X3) is a 3-Channel dataset, prepared by PREPARE3CHDATA (i.e.
@@ -20,6 +20,9 @@ c = struct2cell(p.Results);
 ys12 = smoothCrossCorr(X1(:,:,1:nsegments),X2(:,:,1:nsegments));
 ys13 = smoothCrossCorr(X1(:,:,1:nsegments),X3(:,:,1:nsegments));
 ys23 = smoothCrossCorr(X2(:,:,1:nsegments),X3(:,:,1:nsegments));
+A12 = max(ys12(:))-min(ys12(:));
+A13 = max(ys13(:))-min(ys13(:));
+A23 = max(ys23(:))-min(ys23(:));
 
 %% Plot
 plot(ys12(:),'linewidth',3);
