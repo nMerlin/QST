c9ae11045b86abb8e874d32e4d5e54a0458fddb4
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-02-15 18:54:31 +0100

    seriesCrossCorrelations: Now also plots simulated data.

--- Series/seriesCrossCorrelations.m
+++ Series/seriesCrossCorrelations.m
@@ -3,7 +3,7 @@ function [] = seriesCrossCorrelations(measurements,varargin)
 
 %% Validate and parse input arguments
 p = inputParser;
-defaultPaths = '.';
+defaultPaths = {'.'};
 addParameter(p,'Paths',defaultPaths,@isvector);
 defaultPrepOpts = struct;
 addParameter(p,'PrepOpts',defaultPrepOpts,@isstruct);
@@ -31,19 +31,36 @@ for iPath = 1:nPaths % iterate through different root paths
             ['\d?',num2str(vLos(iMeas)),'-.*?.raw'],'match','once');
         fMeas = regexp(files, ...
             ['\d?',num2str(vMeas(iMeas)),'-.*?.raw'],'match','once');
-        [X1,X2,X3,~,config] = prepare3ChData(fLO,fMeas,prepopts);
+        [X1,X2,X3,~,config] = prepare3ChData(fLO,fMeas,prepopts, ...
+            'CorrRemove','yes');
         [A12,A13,A23] = plotCrossCorrelation(X1,X2,X3);
         % Add additional information to the plot
-        n1 = var(X1(:))-0.5; f1 = config.E725.Piezo1.Frequency_DBL;
-        n2 = var(X2(:))-0.5; f2 = config.E725.Piezo2.Frequency_DBL;
-        n3 = var(X3(:))-0.5; f3 = config.E725.Piezo3.Frequency_DBL;
+        n1 = var(X1(:))-0.5;
+        n2 = var(X2(:))-0.5;
+        n3 = var(X3(:))-0.5;
+        % Read piezo frequencies
+        if config.E725.Piezo1.GeneratorOn_BOOL
+            f1 = config.E725.Piezo1.Frequency_DBL;
+        else
+            f1 = 0;
+        end
+        if config.E725.Piezo2.GeneratorOn_BOOL
+            f2 = config.E725.Piezo2.Frequency_DBL;
+        else
+            f2 = 0;
+        end
+        if config.E725.Piezo3.GeneratorOn_BOOL
+            f3 = config.E725.Piezo3.Frequency_DBL;
+        else
+            f3 = 0;
+        end
         title({['Smoothed Cross-Correlations for f1=',num2str(f1), ...
             ', f2=',num2str(f2),', f3=',num2str(f3),'Hz']; ...
             ['Photon numbers n1=',num2str(n1,3),', n2=',num2str(n2,3), ...
             ', n3=',num2str(n3,3)]; ...
             ['Amplitudes A12=',num2str(A12,3),', A13=',num2str(A13,3), ...
             ', A23=',num2str(A23,3)]});
-        fOut = [datestr(date,'yyyy-mm-dd'),'-CrossCorrs-', ...
+        fOut = [datestr(date,'yyyy-mm-dd'),'-CrossCorrs-CorrRemove-', ...
             fMeas,'.png'];
         fig = gcf;
         fig.PaperUnits = 'centimeters';
@@ -53,6 +70,28 @@ for iPath = 1:nPaths % iterate through different root paths
         dispstat(['Path ',num2str(iPath),' of ',num2str(nPaths), ...
             ' and measurement ',num2str(iMeas),' of ', ...
             num2str(nMeas),'.'],'keepthis');
+        % Simulation
+        nges = n1 + n2 + n3;
+        r1 = sqrt(n1/nges);
+        r2 = sqrt(n2/(nges-n1));
+        [nPulses,nPieces,nSegments] = size(X1);
+        [X1s,X2s,X3s] = simQST(nPulses,nPieces,nSegments,'Channels',3, ...
+            'R1',r1,'R2',r2,'NPhotons',nges,'PhasePeriods',2.1, ...
+            'Frequencies',[f1 f2 f3]);
+        [A12s,A13s,A23s] = plotCrossCorrelation(X1s,X2s,X3s);
+        title({['Simulated Smoothed Cross-Correlations for f1=',num2str(f1), ...
+            ', f2=',num2str(f2),', f3=',num2str(f3),'Hz']; ...
+            ['Photon numbers n1=',num2str(n1,3),', n2=',num2str(n2,3), ...
+            ', n3=',num2str(n3,3)]; ...
+            ['Amplitudes A12=',num2str(A12s,3),', A13=',num2str(A13s,3), ...
+            ', A23=',num2str(A23s,3)]});
+        fOut = [datestr(date,'yyyy-mm-dd'), ...
+            '-Simulated-CrossCorrs-CorrRemove-',fMeas,'.png'];
+        fig = gcf;
+        fig.PaperUnits = 'centimeters';
+        fig.PaperPosition = [0 0 29.7 21];
+        fig.PaperPositionMode = 'manual';
+        print(fOut,'-dpng');
     end
 end
 
