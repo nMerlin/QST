26c27930fd842d2b1b9a30d4c256ba24d274b26a
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-03-15 11:27:36 +0100

    plotG2: Cleaning up.

--- Plot/plotG2.m
+++ Plot/plotG2.m
@@ -1,12 +1,9 @@
-function plotG2(times,values,varargin)
+function plotG2(times,values)
 %PLOTG2 Plot g2 values against recording times (expects SI units)
 %
 % Input Arguments:
 %   times: vector with time axis
 %   values: vector with g2 values
-%
-% Optional Input Arguments:
-%   'Units': Unit of the time axis, e.g. 'ms'.
 
 %% Create plot
 [times,units] = convenientUnits(times,'s');
