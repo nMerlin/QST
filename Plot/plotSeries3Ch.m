58a948e381727eef5677880c730b86e2e9fce45b
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-06-11 16:49:46 +0200

    plotSeries3Ch: New plot type 'Photons'.

--- Plot/plotSeries3Ch.m
+++ Plot/plotSeries3Ch.m
@@ -5,35 +5,53 @@ function plotSeries3Ch(T,varargin)
 p = inputParser;
 defaultSave = false;
 addParameter(p,'Save',defaultSave,@islogical);
+defaultType = 'Delay';
+addParameter(p,'Type',defaultType,@isstr);
 parse(p,varargin{:});
 c = struct2cell(p.Results);
-[saveflag] = c{:};
+[saveflag,typestr] = c{:};
 
-%% Create plot
-delays = T.Delay;
-varX = T.meanVarX;
-plot(delays,varX,'o');
-fo = fitoptions('Method','NonlinearLeastSquares', ...
-    'StartPoint',[-5 0 600 8.3]);
-ft = fittype('a*exp(-(x-b)^2/t^2)+c','options',fo);
-res = fit(delays,varX,ft);
+switch typestr
+    case 'Delay'
+        %% Create plot for delay line series
+        xAxis = T.Delay;
+        varX = T.meanVarX;
+        plot(xAxis,varX,'o','DisplayName', ...
+            ['Minimum: ',num2str(min(varX))]);
+        xlabel('Delay (fs)');
+        
+        % Fit with Gaussian
+        fo = fitoptions('Method','NonlinearLeastSquares', ...
+            'StartPoint',[-5 0 600 8.3]);
+        ft = fittype('a*exp(-(x-b)^2/t^2)+c','options',fo);
+        res = fit(xAxis,varX,ft);
+        hold on;
+        plot(min(xAxis):1:max(xAxis),res(min(xAxis):1:max(xAxis)),'r', ...
+            'DisplayName','Fit');
+        xlim([min(xAxis) max(xAxis)]);
+        hold off;
+    case 'Photons'
+        %% Create plot for series with different photon numbers
+        nX1 = T.nX1; nX2 = T.nX2; nX3 = T.nX3;
+        xAxis = nX1./(nX2+nX3);
+        varX = T.meanVarX;
+        plot(xAxis,varX,'o','DisplayName', ...
+            ['Minimum: ',num2str(min(varX))]);
+        xlabel('n_t/n_{ps}');
+end
+
+%% Plot limits & labels
 hold on;
-plot(-2000:1:2000,res(-2000:1:2000),'r');
-xlim([-2000 2000]);
-
 % Lower Bound
 minVar = compute3ChLimit(T.nX2,T.nX3,T.nX1);
-plot(delays,minVar,'x');
+plot(xAxis,minVar,'x','DisplayName',['Limit ca. ',num2str(min(minVar))]);
 
 % Upper Bound
-plot(delays,T.nX1+0.5,'*');
+plot(xAxis,T.nX1+0.5,'*','DisplayName','Upper Bound');
 
 % Labels
-xlabel('Delay (fs)');
 ylabel('Postselected Variance');
-legend(['Minimum: ',num2str(min(varX))],'Fit', ...
-    ['Limit ca. ',num2str(max(minVar))],'Upper Bound', ...
-    'Location','SouthEast');
+legend('show');
 title('3-Channel Series');
 hold off;
 
