c7eb37283d98cb15255cdab163af66be57296a99
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-06-11 16:26:01 +0200

    plotSeries3Ch: Save option. Bugfixes with variance limits.

--- Plot/plotSeries3Ch.m
+++ Plot/plotSeries3Ch.m
@@ -1,6 +1,15 @@
-function plotSeries3Ch(varargin)
-%PLOTSERIES3CH Plot results from SERIES3CH time series
+function plotSeries3Ch(T,varargin)
+%PLOTSERIES3CH Plot results from SERIES3CH table T.
 
+%% Validate and parse input arguments
+p = inputParser;
+defaultSave = false;
+addParameter(p,'Save',defaultSave,@islogical);
+parse(p,varargin{:});
+c = struct2cell(p.Results);
+[saveflag] = c{:};
+
+%% Create plot
 delays = T.Delay;
 varX = T.meanVarX;
 plot(delays,varX,'o');
@@ -11,21 +20,27 @@ res = fit(delays,varX,ft);
 hold on;
 plot(-2000:1:2000,res(-2000:1:2000),'r');
 xlim([-2000 2000]);
-xlabel('Delay (fs)');
-ylabel('Postselected Variance');
 
 % Lower Bound
-minVar = compute3ChLimit(T.nX1,T.nX2,T.nX3);
+minVar = compute3ChLimit(T.nX2,T.nX3,T.nX1);
 plot(delays,minVar,'x');
 
 % Upper Bound
-plot(delays,T.nX3+0.5,'*');
+plot(delays,T.nX1+0.5,'*');
 
+% Labels
+xlabel('Delay (fs)');
+ylabel('Postselected Variance');
 legend(['Minimum: ',num2str(min(varX))],'Fit', ...
     ['Limit ca. ',num2str(max(minVar))],'Upper Bound', ...
     'Location','SouthEast');
-
+title('3-Channel Series');
 hold off;
+
+%% Write figure to file
+if saveflag
+    saveA5Landscape([datestr(date,'yyyy-mm-dd-'),'plotSeries3Ch']);
+end
 
 end
 
