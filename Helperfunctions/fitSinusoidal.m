01f69732d864730189e74d0003a41731dfdc389d
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-06-12 11:22:55 +0200

    fitSinusoidal: Phase and Periods as optional input parameters.

--- Helperfunctions/fitSinusoidal.m
+++ Helperfunctions/fitSinusoidal.m
@@ -1,6 +1,7 @@
 function [fitParams,fval,exitFlag] = fitSinusoidal(x,y,varargin)
 %FITSINUSOIDAL Fits a sine function to a (x,y) dataset. (x,y) should
-%already be sinusoidal for the fit to work properly.
+%already be sinusoidal for the fit to work properly. The dataset will be
+%sorted according to the x values before fitting.
 %
 % Output Parameters:
 %   FITPARAMS outputs the fit paramters b of b1*sin(2*pi/b2*x+2*pi/b3)+b4
@@ -12,6 +13,10 @@ function [fitParams,fval,exitFlag] = fitSinusoidal(x,y,varargin)
 %   'RemoveLinearOffset': Default is false. If true, a linear trend is
 %       removed from the sine function.
 %   'Plot': Default is 'hide'. If 'show', a graphical output is generated.
+%   'Periods': Number of periods, default is -1 and the algorithm tries to
+%       find the number of periods according to the zero crossings it finds
+%       (experimental).
+%   'Phase': Phase of first datapoint (in rad), default is 0.
 %
 % Adapted from Star Strider:
 % https://www.mathworks.com/matlabcentral/answers/ ...
@@ -19,38 +24,51 @@ function [fitParams,fval,exitFlag] = fitSinusoidal(x,y,varargin)
 
 %% Validate and parse input arguments
 p = inputParser;
+defaultPeriod = -1;
+addParameter(p,'Period',defaultPeriod,@isnumeric);
 defaultPlotOpt = 'hide';
 addParameter(p,'Plot',defaultPlotOpt,@isstr);
 defaultRemoveLinearOffset = false;
 addParameter(p,'RemoveLinearOffset',defaultRemoveLinearOffset,@islogical);
+defaultPhase = 0;
+addParameter(p,'yPhase',defaultPhase,@isnumeric);
 parse(p,varargin{:});
 c = struct2cell(p.Results);
-[plotOpt,rmFlag] = c{:};
+[period,plotOpt,rmFlag,yPhase] = c{:};
 
-% Correcting input data format
-y = y(~isnan(y)); % Remove NaN-entries in y
+%% Correcting input data format
+% Remove NaN-entries
+y = y(~isnan(y));
 x = x(~isnan(x));
+% Ensure row-vectors
 if ~isrow(y)
     y = y';
 end
 if ~isrow(x)
     x = x';
 end
+% Sort according to x values
+[x,sortedI] = sort(x);
+y = y(sortedI);
 
-% Estimate fit paramters
+%% Estimate fit paramters
+% Estimate offset
 yMax = max(y);
 yMin = min(y);
 yRange = (yMax-yMin);
 yZero = y-yMax+(yRange/2);
-zeroIndicator = yZero.* circshift(yZero, [0 1]); ...
-    % Find zero-crossings (ignore first entry in zeroIndicator)
-zerosX = x([1 zeroIndicator(2:end)] <= 0);
-period = 2*mean(diff(zerosX));
-yOffset = yMin + yRange/2; % Estimate offset
-yPhase = mod(-period/zerosX(1),period); % Estimate phase
+yOffset = yMin + yRange/2;
+if period == -1
+    % Estimate periods by finding zero crossings
+    zeroIndicator = yZero.* circshift(yZero, [0 1]);
+    zerosX = x([1 zeroIndicator(2:end)] <= 0);
+    period = 2*mean(diff(zerosX));
+else
+    period = (x(end)-x(1))/period;
+end
 
 % Function to fit
-fitFunction = @(b,x)  b(1).*(sin(2*pi*x./b(2) + 2*pi/b(3))) + b(4);
+fitFunction = @(b,x)  b(1).*(sin(2*pi*x./b(2) + b(3))) + b(4);
 % Least-Squares cost function
 squaresFunction = @(b) sum((fitFunction(b,x) - y).^2);
 % Minimise Least-Squares (with lower boundaries)
@@ -66,10 +84,10 @@ if rmFlag
     [fitParams; 0], [0; 0; -inf; -inf; -inf]);
 end
 
-%%% Plot raw data and fitted function
+%% Plot raw data and fitted function
 if strcmp(plotOpt,'show')
     figure(1)
-    plot(x,y,'b',x,fitFunction(fitParams,x),'r')
+    plot(x,y,'.',x,fitFunction(fitParams,x),'r')
     grid
 end
     
