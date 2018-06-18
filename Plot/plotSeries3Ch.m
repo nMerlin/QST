0667ca686622a36b64da5beba0ef530cc9806251
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-06-18 20:45:19 +0200

    plotSeries3Ch: Cleaner implementation of plots out of input table.

--- Plot/plotSeries3Ch.m
+++ Plot/plotSeries3Ch.m
@@ -12,48 +12,67 @@ c = struct2cell(p.Results);
 [saveflag,typestr] = c{:};
 
 switch typestr
-    case 'Delay'
-        %% Create plot for delay line series
+    case 'DelayMeanVarX'
+        %% Create plot delays versus postselected variance
+        % Plot data
         xAxis = T.Delay;
         varX = T.meanVarX;
-        plot(xAxis,varX,'o','DisplayName', ...
-            ['Minimum: ',num2str(min(varX))]);
+        plot(xAxis,varX,'o','DisplayName','(\Delta Q)^2'); hold on;
         xlabel('Delay (fs)');
         
         % Fit with Gaussian
         fo = fitoptions('Method','NonlinearLeastSquares', ...
             'StartPoint',[-5 0 600 8.3]);
-        ft = fittype('a*exp(-(x-b)^2/t^2)+c','options',fo);
+        ft = fittype('a*exp(-(x-b)^2/(2*s^2))+c','options',fo);
         res = fit(xAxis,varX,ft);
-        hold on;
         plot(min(xAxis):1:max(xAxis),res(min(xAxis):1:max(xAxis)),'r', ...
-            'DisplayName','Fit');
+            'DisplayName',['Gaussian with \sigma=', ...
+            num2str(round(res.s)),' fs']);
         xlim([min(xAxis) max(xAxis)]);
-        hold off;
+        
+        % Lower Bound
+        %minVar = compute3ChLimit(T.nX2,T.nX3,T.nX1);
+        %plot(xAxis,minVar,'x','DisplayName',['Limit ca. ', ...
+        %    num2str(min(minVar))]);
+
+        % Upper Bound
+        %plot(xAxis,T.nX1+0.5,'*','DisplayName','Upper Bound');
+
+        % Labels
+        ylabel('Postselected Variance');
+    case 'DelayDiscAmpl'
+        %% Create plot delays versus amplitude (discretization method)
+        xAxis = T.Delay;
+        yAxis = T.discAmpl;
+        plot(xAxis,yAxis,'o','DisplayName','|\alpha|'); hold on;
+        xlabel('Delay (fs)');
+        ylabel('Coherent Amplitude');
+        xlim([min(xAxis) max(xAxis)]);
+        
+        % Fit with Gaussian
+        fo = fitoptions('Method','NonlinearLeastSquares', ...
+            'StartPoint',[-5 0 600 8.3]);
+        ft = fittype('a*exp(-(x-b)^2/(2*s^2))+c','options',fo);
+        res = fit(xAxis,yAxis,ft);
+        hold on;
+        fitx = min(xAxis):1:max(xAxis);
+        fity = res(min(xAxis):1:max(xAxis));
+        plot(fitx,fity,'r','DisplayName',['Gaussian; \sigma=', ...
+            num2str(round(abs(res.s))),' fs']);
     case 'Photons'
         %% Create plot for series with different photon numbers
         nX1 = T.nX1; nX2 = T.nX2; nX3 = T.nX3;
         xAxis = nX1./(nX2+nX3);
         varX = T.meanVarX;
         plot(xAxis,varX,'o','DisplayName', ...
-            ['Minimum: ',num2str(min(varX))]);
+            ['Minimum: ',num2str(min(varX))]); hold on;
         xlabel('n_t/n_{ps}');
 end
 
-%% Plot limits & labels
-hold on;
-% Lower Bound
-minVar = compute3ChLimit(T.nX2,T.nX3,T.nX1);
-plot(xAxis,minVar,'x','DisplayName',['Limit ca. ',num2str(min(minVar))]);
-
-% Upper Bound
-plot(xAxis,T.nX1+0.5,'*','DisplayName','Upper Bound');
-
-% Labels
-ylabel('Postselected Variance');
-legend('show');
-title('3-Channel Series');
+%% Common figure manipulation
 hold off;
+legend('show');
+
 
 %% Write figure to file
 if saveflag
