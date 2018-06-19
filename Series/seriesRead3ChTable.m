fcefcb516c7c3c0a1608e6fe9de5c55074b51cf2
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-06-19 19:13:02 +0200

    seriesRead3ChTable: Implemented parsing of selection parameters.

--- Series/seriesRead3ChTable.m
+++ Series/seriesRead3ChTable.m
@@ -1,11 +1,22 @@
-function T = seriesRead3ChTable()
+function T = seriesRead3ChTable(selParams)
 %SERIESREADTABLE3CH Load most recent table file 'yyyy-MM-dd-series3Ch.txt'
-% in current folder.
+% in current folder. Output is empty if no table was detected.
+%
+% Usage:
+%   T = seriesRead3ChTable(selParams);
+%
+% Input Arguments:
+%   selParams: Structure with selection parameters for 'selectRegion'.
 
+selStr = '';
+if nargin > 0
+    selStr = selParamsToStr(selParams);
+end
 filestruct = dir('*-series3Ch.txt');
 if ~isempty(filestruct)
     filestring = strjoin({filestruct.name});
-    filedates = regexp(filestring,'([^ ]*)-series3Ch.txt','tokens');
+    filedates = regexp(filestring,'([^ ]*)-',selStr, ...
+        '-series3Ch.txt','tokens');
     filedates = [filedates{:}]';
     filedates = datetime(filedates,'InputFormat','yyyy-MM-dd');
     filenames = {filestruct.name}';
