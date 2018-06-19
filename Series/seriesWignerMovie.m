b14feff63b29dc2281242345bd6b11364fc830b9
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-06-19 09:02:04 +0200

    seriesWignerMovie: Implemented meaningful movie titles, cross-feed of optional input arguments and handling of *-series3Ch.txt table.

--- Series/seriesWignerMovie.m
+++ Series/seriesWignerMovie.m
@@ -1,34 +1,66 @@
-function seriesWignerMovie()
+function seriesWignerMovie(varargin)
 %SERIESWIGNERMOVIE Creates a movie from series of Wigner functions. Is a
 %wrapper function for 'plotMovie'.
+%
+% Optional Input Arguments:
+%   'Image': Default is 'false' (3D plot). If 'true',
+%       create a 2D plot of WF.
+%   'Narrow': Default is 'false'. Narrower axis limits.
 %
 % Prerequisites:
 %   - expects 'WF' variable in the *.mat files in a 'post-data' folder
 
-%% Discover *.mat files
+%% Validate and parse input arguments
+p = inputParser;
+defaultImage = false;
+addParameter(p,'Image',defaultImage,@islogical);
+defaultNarrow = false;
+addParameter(p,'Narrow',defaultNarrow,@islogical);
+parse(p,varargin{:});
+c = struct2cell(p.Results);
+[image,narrow] = c{:};
+
+%% Check data availability and capture data table
 dispstat('','init')
 dispstat('seriesWignerMovie: Checking folder ''post-data''', ...
     'timestamp','keepthis');
-if exist('post-data','dir')
-    filestruct = dir('post-data/*.mat');
-    files = {filestruct.name};
+T = seriesRead3ChTable();
+if ~isempty(T) && ismember('Delay',T.Properties.VariableNames)
+    files = T.Filename;
+    delays = T.Delay;
+    titles = cell(length(delays),1);
+    for iDelay = 1:length(delays)
+        titles{iDelay} = ['Delay: ',num2str(delays(iDelay)),' fs'];
+    end
 else
-    warning('Could not find the ''post-data'' directory!');
+    titles = {};
+    %% Discover *.mat files
+    if exist('post-data','dir')
+        filestruct = dir('post-data/*.mat');
+        files = {filestruct.name};
+    else
+        warning('Could not find the ''post-data'' directory!');
+    end
 end
 
 %% Capture the available WF variables
 allWF = {};
 for iFile = 1:length(files)
-    dispstat(['seriesWignerMovie: Loading ','post-data/',files{iFile}], ...
-        'timestamp');
-    load(['post-data/',files{iFile}]);
+    ss = strsplit(files{iFile},'.');
+    dispstat(['seriesWignerMovie: Loading ','post-data/',ss{1}, ...
+        '-postselection.mat'],'timestamp');
+    load(['post-data/',ss{1},'-postselection.mat']);
     if exist('WF','var')
         allWF(end+1) = {WF};
         clear WF;
+    else
+        titles{iFile} = {};
     end
 end
+titles = titles(~cellfun(@isempty,titles));
 
 %% Generate Movie
-plotMovie(@(wigfun,params) plotWigner(wigfun,params),allWF);
+plotfun = @(wigfun,params) plotWigner(wigfun,params,varargin{:});
+plotMovie(plotfun,allWF,'Titles',titles);
 
 end
