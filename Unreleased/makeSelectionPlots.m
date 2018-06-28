bed1348d0a0169686f3a580266c88f9a14ef861e
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-06-28 12:04:55 +0200

    fix error when deleting pdfs

--- Unreleased/makeSelectionPlots.m
+++ Unreleased/makeSelectionPlots.m
@@ -77,7 +77,7 @@ if cleanPdfs
             'SelectionParameters',listOfParams(iParams));
     end
     listRadiusPdfs = dir([pdfpath,'*-RadiusDiscAmpl*']);
-    cellfun(@(x) delete([pdfpath,x]),{listRadiusPlots.name});
+    cellfun(@(x) delete([pdfpath,x]),{listRadiusPdfs.name});
 end
 
 end
