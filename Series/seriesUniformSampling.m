55f56c3e37af4dc9b647f94d0cf6a82356913e45
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-07-02 11:56:36 +0200

    implement full 'seriesUniformSampling'

--- Series/seriesUniformSampling.m
+++ Series/seriesUniformSampling.m
@@ -1,7 +1,24 @@
 function [uniformX,uniformTheta] = seriesUniformSampling(selX,selTheta,varargin)
 %SERIESUNIFORMSAMPLING Uniformly sample in 'selTheta'.
 
+%% Validate and parse input arguments
+p = inputParser;
+defaultNBins = 100;
+addParameter(p,'NBins',defaultNBins,@isnumeric);
+parse(p,varargin{:});
+c = struct2cell(p.Results);
+[nBins] = c{:};
 
+[thetaN,~,bin] = histcounts(selTheta,nBins);
+nMin = min(thetaN);
+[uniformX,uniformTheta] = deal(zeros(nBins*nMin,1));
+for iBin = 1:nBins
+    r = randi(thetaN(iBin),nMin,1);
+    X = selX(bin==iBin);
+    Theta = selTheta(bin==iBin);
+    uniformX(((iBin-1)*nMin+1):iBin*nMin) = X(r);
+    uniformTheta(((iBin-1)*nMin+1):iBin*nMin) = Theta(r);
+end
 
 end
 
