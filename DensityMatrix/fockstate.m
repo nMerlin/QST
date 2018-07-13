58208abe22c9b23a1ecb8f1f3434796249c115c6
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-07-13 13:41:20 +0200

    move fockstate to correct directory

--- DensityMatrix/fockstate.m
+++ DensityMatrix/fockstate.m
@@ -1,5 +1,5 @@
 function fockstate = fockstate(n,x)
-%HERMITEGAUSSIAN computes the number state wavefunction <x|n>.
+%HERMITEGAUSSIAN computes the number state wavefunction <n,x>.
 fockstate=polyval(hermitePoly(n),x).*(pi^(-0.25)).* ...
     (1/sqrt((2^n)*factorial(n))).*exp(-0.5*(x.^2));
 end
