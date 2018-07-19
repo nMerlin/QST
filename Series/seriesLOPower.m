6467ef10bdfd0de382ad8a8699a06c3e2b90efc0
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-07-19 12:12:18 +0200

    rename seriesHDPower to seriesLOPower

---
+++ Series/seriesLOPower.m
@@ -0,0 +1,47 @@
+function seriesHDPower(varargin)
+%SERIESHDPOWER creates a plot HD-Output-Power vs. LO-Power
+
+%% Validate and parse input arguments
+p = inputParser;
+defaultPrepOpts = struct;
+addParameter(p,'PrepOpts',defaultPrepOpts,@isstruct);
+parse(p,varargin{:});
+c = struct2cell(p.Results);
+[prepopts] = c{:};
+
+%% Read file names and extract LO power
+filestruct = dir('raw-data/*.raw');
+filestring = strjoin({filestruct.name});
+fPower = regexpi(filestring,'[^ ]*mW[^ ]*.raw','match');
+tokMeas = regexpi(strjoin(fPower),'\<(\d*)-','tokens');
+nMeas = cellfun(@str2num,[tokMeas{:}]);
+tokPow = regexpi(strjoin(fPower),'([0123456789.])*mW','tokens');
+pMeas = cellfun(@str2num,[tokPow{:}]);
+
+%% Compute signal powers
+[powerHD,stdPowerHD] = deal(zeros(1,length(fPower)));
+for i=1:length(fPower)
+    data8bit = load8BitBinary(fPower{i},'dontsave');
+    vars = var(double(data8bit));
+    powerHD(i) = mean(vars);
+    stdPowerHD(i) = std(vars);
+end
+
+p = polyfit(pMeas,powerHD,1);
+f = polyval(p,pMeas);
+errorbar(pMeas,powerHD,stdPowerHD,'o');
+hold on;
+plot(pMeas,f,'-');
+hold off;
+xlabel('LO power (mW)');
+ylabel('HD Output Power (arb. unit)');
+title('LO power series');
+saveA5Landscape('seriesHDPower');
+PowerLO = pMeas';
+OutputPowerHD = powerHD';
+StandardDeviation = stdPowerHD';
+T = table(PowerLO,OutputPowerHD,StandardDeviation);
+writetable(T,'seriesHDPower');
+
+end
+
