d12b3da26e07fc1f6a6dec2d77230b119a3b74fc
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-07-27 19:39:12 +0200

    fix bug introduced earlier

--- DensityMatrix/computeProjector1D.m
+++ DensityMatrix/computeProjector1D.m
@@ -3,7 +3,7 @@ function [ PI1D ] = computeProjector1D( X, theta, maxFockState )
 
 PI1D =zeros(maxFockState+1,length(X));
 for n=0:maxFockState
-    PI1D(n+1,:) =fockstate2(n,X) .*exp(1i*n*theta);
+    PI1D(n+1,:) =fockstate(n,X) .*exp(1i*n*theta);
 end
 
 end
