219e98834532346816f5c950b32409dc9980d7f5
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-08-22 12:32:45 +0200

    add complex declaration

--- DensityMatrix/computeProjector1D.m
+++ DensityMatrix/computeProjector1D.m
@@ -1,7 +1,7 @@
 function [ PI1D ] = computeProjector1D( X, theta, maxFockState )
 %COMPUTEPROJECTOR1D <n|X,theta> for n = 0:maxFockState
 
-PI1D =zeros(maxFockState+1,length(X));
+PI1D = complex(zeros(maxFockState+1,length(X)));
 for n=0:maxFockState
     PI1D(n+1,:) =fockstate(n,X) .*exp(1i*n*theta);
 end
