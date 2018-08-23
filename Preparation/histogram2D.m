a8acb1e3afb6d3b443bb1a432379bb9dd8414450
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-08-23 14:04:03 +0200

    accept externally given bins

--- Preparation/histogram2D.m
+++ Preparation/histogram2D.m
@@ -4,10 +4,14 @@ function [H, binsA, binsB] = histogram2D(A,B,varargin)
 % Optional input arguments:
 %   histogram2D(A,B,'plot'): Compute the histogram and plot its heat map.
 %       Default value is 'noplot'.
-%   histogram2D(A,B,~,binsA,binsB): Have exactly binsA bins in the
-%       A-direction and binsB bins in the B-direction. Default value is 0
-%       and will result in as many bins as are necessary to retain existing
-%       discretization (only useful for previously discretized data).
+%   histogram2D(A,B,~,binsA,binsB):
+%       If binsA and binsB are scalars: Have exactly binsA bins in the
+%       A-direction and binsB bins in the B-direction. Default value is
+%       1000. 0 will result in as many bins as are necessary to retain
+%       existing discretization (only useful for previously discretized
+%       data).
+%       If binsA and binsB are vectors: Assume that they describe already
+%       the binning.
 
 %% Handle optional input arguments
 nVarargin = length(varargin);
@@ -17,17 +21,26 @@ optArgs(1:nVarargin) = varargin;
 
 %% Calculate binning and histogram
 A = A(:); B = B(:);
-if nBinsA == 0
-    [binsA, nBinsA] = minBins(A,'sym');
+if length(nBinsA)==1 && length(nBinsB)==1
+    if nBinsA == 0
+        [binsA, nBinsA] = minBins(A,'sym');
+    else
+        maxVal = max(-min(A),max(A));
+        binsA = linspace(-maxVal,maxVal,nBinsA);
+    end
+    if nBinsB == 0
+        [binsB, nBinsB] = minBins(B,'sym');
+    else
+        maxVal = max(-min(A),max(A));
+        binsB = linspace(-maxVal,maxVal,nBinsB);
+    end
+elseif length(nBinsA)>1 && length(nBinsB)>1
+    binsA = nBinsA;
+    binsB = nBinsB;
+    nBinsA = length(binsA);
+    nBinsB = length(binsB);
 else
-    maxVal = max(-min(A),max(A));
-    binsA = linspace(-maxVal,maxVal,nBinsA);
-end
-if nBinsB == 0
-    [binsB, nBinsB] = minBins(B,'sym');
-else
-    maxVal = max(-min(A),max(A));
-    binsB = linspace(-maxVal,maxVal,nBinsB);
+    error('Mixed input data types for nBinsA and nBinsB!');
 end
 % map A and B to bin indices
 Ai = round(interp1(binsA,1:nBinsA,A,'linear','extrap'));
