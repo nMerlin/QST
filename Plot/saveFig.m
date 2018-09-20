ba9b72160ffbbd6f1998663c2d1d9ec5d3291606
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-09-20 11:49:28 +0200

    add wrapper function for export_fig

---
+++ Plot/saveFig.m
@@ -0,0 +1,18 @@
+function saveFig(filename)
+%SAVEFIG Wrapper for export_fig
+
+if ~isempty(filename)
+    set(gcf,'Color','w');
+    [path,name,ext] = fileparts(filename);
+    switch ext
+        case '.jpg'
+            filenameJpg = [path,name,'.jpg'];
+            export_fig(sprintf('%s',filenameJpg),'-jpg','-r600');
+        case '.pdf'
+            filenamePdf = [path,name,'.pdf'];
+            export_fig(sprintf('%s',filenamePdf),'-pdf','-painters');
+    end
+end
+
+end
+
