6e75b5337d9f8d03497849122f04b7b357879837
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-09-28 11:22:45 +0200

    transform from alpha to q-space

--- Wigner/thermHusimi.m
+++ Wigner/thermHusimi.m
@@ -1,10 +1,20 @@
-function [ QF ] = thermHusimi(q,p,nPhotons,disc)
+function [ QF ] = thermHusimi(q,p,nPhotons,varargin)
 %THERMHUSIMI Returns Q(q,p) for a thermal state with NPHOTONS.
 %   Detailed explanation goes here
 
+%% Validate and parse input arguments
+parser = inputParser;
+defaultNorm = 1/sqrt(2); % sqrt([q,p]/(2*i))
+addParameter(parser,'Norm',defaultNorm,@isnumeric);
+parse(parser,varargin{:});
+c = struct2cell(parser.Results);
+[norm] = c{:};
+
+disc = mean(diff(q));
 QF = zeros(length(q),length(p));
 for iP = 1:length(p)
-    QF(:,iP)=disc^2*1/(pi*(nPhotons+1))*exp(-(q.^2+p(iP).^2)/(nPhotons+1));
+    QF(:,iP)=disc^2*(1/norm)^2*1/(pi*(nPhotons+1))* ...
+        exp(-((q*norm).^2+(p(iP)*norm).^2)/(nPhotons+1));
 end
 %HF = HF./sum(sum(HF)); % Usually not necessary
 
