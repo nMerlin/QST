424ca61091857a2372cdf53b15a6a91f758b25c8
Johannes Thewes <johannes.thewes@tu-dortmund.de>
2018-09-28 11:48:32 +0200

    formulation in alpha space

--- Wigner/thermWigner.m
+++ Wigner/thermWigner.m
@@ -1,23 +1,25 @@
 function WF = thermWigner( q, p, nPhotons, varargin )
 %THERMWIGNER Returns W(q,p) for a thermal state with NPHOTONS photons
 %
-%   Important: Normalization A = 1/sqrt(2) for quadrature operator q =
-%   A*(a^dagger + a) is used.
+% Formulation is in alpha-space but choice of normalization has to be taken
+% into account.
 
 %% Validate and parse input arguments
 parser = inputParser;
+defaultNorm = 1/sqrt(2); % sqrt([q,p]/(2*i))
+addParameter(parser,'Norm',defaultNorm,@isnumeric);
 defaultTheta = 0; % Rotation angle in degrees
 addParameter(parser,'Theta',defaultTheta,@isnumeric);
 parse(parser,varargin{:});
 c = struct2cell(parser.Results);
-[theta] = c{:};
+[norm,theta] = c{:};
 
 WF = zeros(length(q),length(p));
 for iP = 1:length(p)
     qrot = q*cosd(theta)-p(iP)*sind(theta);
     prot = q*sind(theta)+p(iP)*cosd(theta);
-    WF(:,iP) = 1/pi*1/(2*nPhotons+1)* ...
-        exp(-(qrot.^2+prot.^2)/(2*nPhotons+1));
+    WF(:,iP) = (1/norm)^2*2/pi*1/(2*nPhotons+1)* ...
+        exp(-2*((qrot*norm).^2+(prot*norm).^2)/(2*nPhotons+1));
 end
 WF = WF./sum(sum(WF)); % Renorm (necessary due to discretization)
 
