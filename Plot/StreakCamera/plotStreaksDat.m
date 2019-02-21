4d3752b1534d53e9c9943e69df7636b84e5e3dd6
CarolinLueders <carolin.lueders@hotmail.de>
2019-02-21 16:24:22 +0100

    plotStreaksDat.m: use .dat file

---
+++ Plot/StreakCamera/plotStreaksDat.m
@@ -0,0 +1,62 @@
+function plotStreaksDat
+% for a series of streak measurements located in folder 'raw-data' as .dat
+% files.
+
+
+%% Create data overview
+dataStruct = struct('filename',{});
+dataStructBackground = struct('filename',{},'number',{});
+
+rawDataContents = dir('raw-data');
+
+%% Signal-files
+for name = {rawDataContents.name}
+    % Loop only over Signal files
+    filename = cell2mat(name);
+    if not(isempty(regexpi(filename,'background.dat','match')))...
+            || isempty(regexpi(filename,'.dat','match'))
+        continue
+    end
+    
+    % Fetch number of measurement
+    numberToken = regexpi(filename,'^([0123456789]*)-','tokens');
+    number = str2double(cell2mat(numberToken{1}));
+    dataStruct(number).filename = filename;
+    
+end
+
+%% Background-files
+for name = {rawDataContents.name}
+    % Loop only over background files
+    filename = cell2mat(name);
+    if isempty(regexpi(filename,'background.dat','match'))
+        continue
+    end
+    
+    % Fetch number of measurement
+    numberToken = regexpi(filename,'^([0123456789]*)-','tokens');
+    number = str2double(cell2mat(numberToken{1}));
+    dataStructBackground(number).filename = filename;
+    dataStructBackground(number).number = number;
+end
+BGnumbers = cell2mat({dataStructBackground.number});
+
+%% process the data
+for number = 1:size(dataStruct,2)
+
+    filenameSIG = dataStruct(number).filename;
+    if isempty(filenameSIG)
+        continue
+    end    
+   
+    %find adequate BG-file
+    BGnumber = min(BGnumbers(BGnumbers>=number)); %background was measured after signal
+    filenameBG = dataStructBackground(BGnumber).filename;
+
+    plotStreak( filenameSIG, filenameBG );
+       
+end
+
+
+
+end
