60fbcb6f341ae83f1326009a86310507c305c7ec
CarolinLueders <carolin.lueders@hotmail.de>
2019-05-17 09:55:13 +0200

    g2/g22ChvsDelay.m: get delay from filename

--- g2/g22ChvsDelay.m
+++ g2/g22ChvsDelay.m
@@ -21,7 +21,7 @@ for name = {rawDataContents.name}
     % Loop only over *LOwithDL.raw files
     filename = cell2mat(name);
     if not(isempty(regexpi(filename,'.raw.','match')))...
-            ||(isempty(regexpi(filename,'LOwithDL','match')))
+            ||(isempty(regexpi(filename,'LOasSIG','match')))
         continue
     end
     
@@ -31,7 +31,8 @@ for name = {rawDataContents.name}
     dataStruct(number).filename = filename;
     
     % Get Delay
-    delayToken = regexpi(filename,'DL-([0123456789,]*)','tokens');
+    %delayToken = regexpi(filename,'DL-([0123456789,]*)','tokens');
+    delayToken = regexpi(filename,'-([0123456789,]*)mm','tokens');
     dataStruct(number).Delay = str2double(strrep(cell2mat(delayToken{1}),',','.'));
 end
 
@@ -70,40 +71,44 @@ for number = 1:size(dataStruct,2)
      dispstat(['PrepareData number ' num2str(number)],...
         'timestamp','keepthis','notquiet');
     
-    % First channel
-    [X1,piezoSign1] = prepare1ChData(filenameLO,filenameSIG,'Channel',1);
-    theta1 = computePhase(X1,ones(size(X1)),piezoSign1,'Period',period1);
+    % prepare data for nonrandom phase
+    [X1, X2, ~, piezoSign, ~] = prepareData(filenameLO,filenameSIG,'Channels',1:2,'Offset','global','Piezo','yes');
+    % First channel      
+    theta1 = computePhase(X1,ones(size(X1)),piezoSign,'Period',period1);
     % g2 estimation
     nG2 = 1;
     [g2values,nPhotons] = deal(zeros(nG2,1));
     for iG2=1:nG2
-        [uniformX1,uniformTheta1] = seriesUniformSampling(X1(:),theta1(:),'NBins',100);
+        [uniformX1,uniformTheta1] = seriesUniformSampling(X1(:),theta1(:),'NBins',200);
         [g2values(iG2),nPhotons(iG2)] = g2(uniformX1,length(uniformX1));
     end
     disp(['Delay: ',num2str(dataStruct(number).Delay)]) 
     disp(['First g2 of Channel 1: ',num2str(g2values(1))])
     disp(['First nPhotons: ',num2str(nPhotons(1))])
+    G2Ch1 = g2values(1);
     dataStruct(number).G2Ch1 = g2values(1);
+    NPhotonsCh1 = nPhotons(1);
     dataStruct(number).NPhotonsCh1 = nPhotons(1);
     %save(strcat(num2str(i+1,'%02d'),'-LOwithSIG-DL',num2str(Delays(i)),'-Ch1.mat'),'X1','piezoSign1','theta1','uniformX1','uniformTheta1');
 
     % second channel
-    [X2,piezoSign2] = prepare1ChData(filenameLO,filenameSIG,'Channel',2);
-    theta2 = computePhase(X2,ones(size(X2)),piezoSign2,'Period',period2); 
+    theta2 = computePhase(X2,ones(size(X2)),piezoSign,'Period',period2); 
     % g2 estimation
     nG2 = 1;
     [g2values,nPhotons] = deal(zeros(nG2,1));
     for iG2=1:nG2
-        [uniformX2,uniformTheta2] = seriesUniformSampling(X2(:),theta2(:),'NBins',100);
+        [uniformX2,uniformTheta2] = seriesUniformSampling(X2(:),theta2(:),'NBins',200);
         [g2values(iG2),nPhotons(iG2)] = g2(uniformX2,length(uniformX2));
     end
     disp(['Delay: ',num2str(dataStruct(number).Delay)]) 
     disp(['First g2 of Channel 2: ',num2str(g2values(1))])
     disp(['First nPhotons: ',num2str(nPhotons(1))])
+    G2Ch2 = g2values(1);
     dataStruct(number).G2Ch2 = g2values(1);
+    NPhotonsCh2 = nPhotons(1);
     dataStruct(number).NPhotonsCh2 = nPhotons(1);
     %save(strcat(num2str(i+1,'%02d'),'-LOwithSIG-DL-',num2str(Delays(i)),'-Ch2.mat'),'X2','piezoSign2','theta2','uniformX2','uniformTheta2');
-
+    
     %correlation between both channels
     %theta12 = computePhaseFixMean(X1,X2,piezoSign1,'Period',4);
     %theta12 = computePhase(X1,X2,piezoSign1,'Period',2);
@@ -112,12 +117,16 @@ for number = 1:size(dataStruct,2)
     nG2 = 1;
     [g2values] = deal(zeros(nG2,1));
     for iG2=1:nG2
-        [uniformX12,uniformTheta12] = seriesUniformSampling(X1(:)+X2(:),theta12(:),'NBins',100);
+        [uniformX12,uniformTheta12] = seriesUniformSampling(X1(:)+X2(:),theta12(:),'NBins',200);
         g2values(iG2) = g22Ch(uniformX1, uniformX2, uniformX12,length(uniformX1),length(uniformX2),length(uniformX12));
     end
     disp(['Delay: ',num2str(dataStruct(number).Delay)])
     disp(['First g2 for two channels: ',num2str(g2values(1))])
+    G2Ch1Ch2 = g2values(1);
     dataStruct(number).G2Ch1Ch2 = g2values(1);
+    
+    save(strcat(filenameSIG, '.mat'),'X1','X2','piezoSign','theta1','theta2','uniformX1','uniformX2','uniformTheta1','uniformTheta2',...
+    'theta12','G2Ch1','G2Ch2','NPhotonsCh1','NPhotonsCh2','G2Ch1Ch2');
    
 end
 
@@ -132,9 +141,6 @@ G2vsDLCh1Ch2 = cell2mat({dataStruct.G2Ch1Ch2});
 
 save(['G2-Ch1Ch2-period1-' num2str(period1) '-period2-' num2str(period2) '.mat'],...
     'Delays','NPhvsDLCh1','G2vsDLCh1','NPhvsDLCh2','G2vsDLCh2','G2vsDLCh1Ch2');
-
-theta12 = mod(theta2 - theta1,2*pi); %Why 4pi?
-histogram(theta12)
 
 figure(1),plot((Delays-Delays(1))*10/3/1*2,NPhvsDLCh1,(Delays-Delays(1))*10/3/1*2,NPhvsDLCh2);
 figure(2),plot((Delays-Delays(1))*10/3/1*2,G2vsDLCh1,(Delays-Delays(1))*10/3/1*2,G2vsDLCh2);
