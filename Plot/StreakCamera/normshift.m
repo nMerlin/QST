2e6ef560d0964863dfdc2bee95c5e947a9c5661a
CarolinLueders <carolin.lueders@hotmail.de>
2019-12-17 12:42:23 +0100

    Plot/StreakCamera/normshift.m: New

---
+++ Plot/StreakCamera/normshift.m
@@ -0,0 +1,14 @@
+%%normalize and shift
+    function [Imax,normInt,normP] = normshift(timeFunc,IntFunc)
+        %make spline interpolation
+        p = csaps(timeFunc, IntFunc,1e-4,timeFunc);
+        %get max and startpoint 
+        [maxP,Imax]=max(p);
+        %startindex = min(find(p>0));
+        % normalize
+        normInt = IntFunc/maxP;
+        normP = p/maxP;
+        %shift time 
+    %     shiftedTime = time(startindex-10:end);
+    %     shiftedInt = time(startindex-10:end);
+    end
