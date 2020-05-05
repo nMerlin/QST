72f0e7e3db4c82b33580cacfcc45414bd6e3ec54
CarolinLueders <carolin.lueders@hotmail.de>
2020-05-05 17:27:48 +0200

    Wigner/HusimiFromWigner.m: norm as a parameter

--- Wigner/HusimiFromWigner.m
+++ Wigner/HusimiFromWigner.m
@@ -1,17 +1,22 @@
 function [HS,pq] = HusimiFromWigner(WF,varargin)
 % computes the Husimi Q function (HS) from a given Wigner function WF. WF must
-% be a matrix, e.g. computed with mainWignerFromRho. 
+% be a quadratic matrix, e.g. computed with mainWignerFromRho. 
+% Formula from equation (6) from V. A. Andreev: A TRANSFORMATIONAL
+% PROPERTY..., Theoretical and Mathematical Physics, 166(3): 356–368 (2011)
+% https://link.springer.com/content/pdf/10.1007/s11232-011-0028-8.pdf
 % Optional Input Arguments:
 %   'PQ': Specify p axis. Default is -20:0.125:20, as this is used in
 %   mainWigner. 
 
 %% Validate and parse input arguments
-p = inputParser;
+parser = inputParser;
 defaultPQ = -20:0.125:20;
-addParameter(p,'PQ',defaultPQ,@isvector);
-parse(p,varargin{:});
-c = struct2cell(p.Results);
-[pq] = c{:};
+addParameter(parser,'PQ',defaultPQ,@isvector);
+defaultNorm = 1/sqrt(2); % sqrt([q,p]/(2*i))
+addParameter(parser,'Norm',defaultNorm,@isnumeric);
+parse(parser,varargin{:});
+c = struct2cell(parser.Results);
+[norm,pq] = c{:};
 
 %% prepare p, q axis
 [p,q] = deal(pq);
@@ -34,13 +39,13 @@ HS = zeros(size(WFs));
 %% evaluate each value of the Husimi matrix
 for x = 1:maxX   
     for y = 1:maxY
-        gaussian = exp(-( q(y) - qMatrix).^2 - (p(x) - pMatrix).^2 );
+        gaussian = exp(-2/(2*norm)^2 * (( q(y) - qMatrix).^2 + (p(x) - pMatrix).^2) );
         Conv = WFs.*gaussian;
         HS(x,y) = sum(sum(Conv));        
     end;
 end;
 
-HS = HS/pi;
+HS = HS * 2/(2*norm)^2/pi;
 HS = HS/sum(sum(HS));
 
 end
