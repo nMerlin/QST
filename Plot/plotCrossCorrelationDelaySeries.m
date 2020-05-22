9d2520dc80a60e6a649f6998daf0f1096b88714c
CarolinLueders <carolin.lueders@hotmail.de>
2020-05-22 17:01:01 +0200

    Plot/plotCrossCorrelationDelaySeries.m: 1st try

---
+++ Plot/plotCrossCorrelationDelaySeries.m
@@ -0,0 +1,93 @@
+function [ dataStruct] = plotCrossCorrelationDelaySeries(varargin)
+%DLSERIES Batch processing of series of g2 measurements, with already
+%computed quadratures that are in the folder 'Quadratures'
+%Computes g2 either time resolved (set 'G2method', 'time') or photon number
+%resolved (set 'G2method', 'bins').
+%   'Weight': If set 'yes', g2 from the time resolved method is weighted 
+%   with the respective squared photon numbers. 
+
+% Optional input arguments
+%% Validate and parse input arguments
+p = inputParser;
+defaultNSegments = 2;
+addParameter(p,'NSegments',defaultNSegments,@isnumeric);
+parse(p,varargin{:});
+c = struct2cell(p.Results);
+[nsegments] = c{:};
+
+%% Variables
+dataStruct = struct('filename',{},'delay',{},'A12',{},'A13',{}, 'A23',{});
+
+%% Create data overview
+dispstat('','init');
+dispstat('Checking filenames ...','timestamp','keepthis');
+%Contents = dir('Quadratures');
+Contents = dir('mat-data');
+name = {Contents.name};
+
+for iStruct =  1:length(Contents) 
+    %get filename
+    filename = cell2mat(name(iStruct));
+    if strcmp(filename,'.') || strcmp(filename,'..') || strcmp(filename,'.txt')
+        continue
+    end
+    dataStruct(iStruct).filename = filename;
+    % get delay; from the file name format xx-yymm, where xx is the
+    % filenumber and yy is the delay which can also be negative and start
+    % with a minus sign
+    delayToken = regexpi(filename,'([-0123456789,-]*)mm','tokens');
+    delay = cell2mat(delayToken{1});
+    numberToken = regexpi(delay,'^([0123456789,]*)-','tokens'); 
+    number = cell2mat(numberToken{1});
+    delay = strrep(delay,[number '-'],'');
+    delay = strrep(delay,',','.');
+    dataStruct(iStruct).delay = str2double(delay);
+    
+    %%% Load data
+    %cd('Quadratures');
+    cd('mat-data');
+    load(filename);
+    cd('..');
+    
+    % Plot crosscorrelation
+    dispstat('Plot cross corr ...','timestamp','keepthis');
+    [A12,A13,A23] = plotCrossCorrelation(X1, X2, X3, filename, 'Nsegments',nsegments);
+    
+    dataStruct(iStruct).A12 = A12;
+    dataStruct(iStruct).A13 = A13;
+    dataStruct(iStruct).A23 = A23;
+
+end % iStruct
+delay = cell2mat({dataStruct.delay});
+A12 = cell2mat({dataStruct.A12});
+A13 = cell2mat({dataStruct.A13});
+A23 = cell2mat({dataStruct.A23});
+
+c = 299792458; % in m/s
+time = 2*delay/1000/c; %delay in mm
+save('CrossCorrDelaySeries.mat','delay','time','A12','A13','A23');
+
+plot(delay,A12,'o-','linewidth',3); hold on;
+plot(delay,A13,'o-','linewidth',3);
+plot(delay,A23,'o-','linewidth',3);
+legend('X1*X2','X1*X3','X2*X3');
+graphicsSettings;
+xlabel('delay (mm)');
+ylabel('visibility');
+savefig('Cross-Correlations-vs-Delay.fig');
+print('Cross-Correlations-vs-Delay.png','-dpng','-r300');
+clf();
+
+plot(time*10^12,A12,'o-','linewidth',3); hold on;
+plot(time*10^12,A13,'o-','linewidth',3);
+plot(time*10^12,A23,'o-','linewidth',3);
+legend('X1*X2','X1*X3','X2*X3');
+graphicsSettings;
+xlabel('time delay (ps)');
+ylabel('visibility');
+savefig('Cross-Correlations-vs-time.fig');
+print('Cross-Correlations-vs-time.png','-dpng','-r300');
+clf();
+
+end % function
+
