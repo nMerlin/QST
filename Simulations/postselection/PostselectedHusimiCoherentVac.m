8b5e9a48f6a399cc674cc6e73bf083ec0ffdd81c
CarolinLueders <carolin.lueders@hotmail.de>
2020-07-10 12:40:13 +0200

    Simulations/postselection/PostselectedHusimiCoherentVac.m

--- Simulations/postselection/PostselectedHusimiCoherentVac.m
+++ Simulations/postselection/PostselectedHusimiCoherentVac.m
@@ -1,6 +1,8 @@
 function PSHusimi = PostselectedHusimiCoherentVac(Q2,P2,n,Transmission)
 %Calculates Wigner function at position (Q1,P1) for mixing coherent light of photon number n and
-%vacuum, then performing postselection on the Q outcome in channel 2
+%vacuum, then performing postselection on the Q outcome in channel 2(Q2,
+%P2 correspond to the postselection channel); they are scaled with sqrt(2)
+%because the postselection arm is divided into two channels. 
 % Using n is a stub. Giving the detailed quadratures in both directions
 % would be better.
 % Transmission gives transmission coefficient of the beam splitter
@@ -18,7 +20,7 @@ QuadVals=P2min:P2res:P2max;
 
 [xx,yy] = meshgrid(QuadVals,QuadVals);
 
-[ModQ1,ModP1,ModQ2,ModP2]=InverseBSTrafo( xx,yy,Q2,P2,Transmission );
+[ModQ1,ModP1,ModQ2,ModP2]=InverseBSTrafo( xx,yy,sqrt(2)*Q2,sqrt(2)*P2,Transmission );
 
 
     %%%%COHERENT AND VACUUM
