e1da4683a364a4589f2545a7f92af769970e4d32
CarolinLueders <carolin.lueders@hotmail.de>
2020-07-10 12:41:33 +0200

    Simulations/postselection/PostselectedHusimiFockVac.m: Q2 scaled with sqrt(2)

--- Simulations/postselection/PostselectedHusimiFockVac.m
+++ Simulations/postselection/PostselectedHusimiFockVac.m
@@ -1,6 +1,8 @@
 function PSHusimi = PostselectedHusimiFockVac(Q2,P2,n,Transmission)
 %Calculates Wigner function at position (Q1,P1) for mixing a Fock state of photon number n and
-%vacuum, then performing postselection on the Q outcome in channel 2
+%vacuum, then performing postselection on the Q outcome in channel 2 (Q2,
+%P2 correspond to the postselection channel); they are scaled with sqrt(2)
+%because the postselection arm is divided into two channels. 
 %
 % Transmission gives transmission coefficient of the beam splitter
 %
@@ -17,7 +19,7 @@ QuadVals=P2min:P2res:P2max;
 
 [xx,yy] = meshgrid(QuadVals,QuadVals);
 
-[ModQ1,ModP1,ModQ2,ModP2]=InverseBSTrafo( xx,yy,Q2,P2,Transmission );
+[ModQ1,ModP1,ModQ2,ModP2]=InverseBSTrafo( xx,yy,sqrt(2)*Q2,sqrt(2)*P2,Transmission );
 
 
     %%%%FOCK AND VACUUM
