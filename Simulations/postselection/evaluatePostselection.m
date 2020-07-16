692acdd868de57dc736b7e7017afcd744996bee8
CarolinLueders <carolin.lueders@hotmail.de>
2020-07-16 14:51:38 +0200

    Simulations/postselection/evaluatePostselection.m: some changes

--- Simulations/postselection/evaluatePostselection.m
+++ Simulations/postselection/evaluatePostselection.m
@@ -1,4 +1,4 @@
-function [A_c, Var_c, n_c] = evaluatePostselection(nTotal,r1,r2,MaxQuad,Resolution,varargin)
+function [A_c, Var_q,Var_p, n_c] = evaluatePostselection(nTotal,r1,r2,MaxQuad,Resolution,varargin)
 % makes simulations of postselection outcomes.
 % Right now it only works for phase averaged displaced thermal states.
 % nTotal: total photon number of input signal state
@@ -21,9 +21,15 @@ defaultThickness = 0;
 addParameter(p,'Thickness',defaultThickness,@isnumeric);
 defaultPhaseAverage = false;
 addParameter(p,'PhaseAverage',defaultPhaseAverage,@islogical);
+defaultPhaseUncertainty = false;
+addParameter(p,'PhaseUncertainty',defaultPhaseUncertainty,@islogical);
+defaultNDisc = 100;
+addParameter(p,'NDisc',defaultNDisc,@isnumeric);
+defaultState = 'PhaseAveragedDisplacedThermal';
+addParameter(p,'State',defaultState,@isstr);
 parse(p,varargin{:});
 c = struct2cell(p.Results);
-[phaseAverage,thickness,Transmission] = c{:};
+[A_ps,nDisc,phaseAverage,phaseUncertainty,state,thickness,Transmission] = c{:};
 
 if phaseAverage
     theta = 0:0.1:2*pi;
@@ -31,21 +37,45 @@ else
     theta = 0;
 end
 
-[A_c, Var_c, n_c] = deal(zeros(length(Transmission),length(A_ps)));
+if phaseUncertainty
+    lowerLim = -pi/2;
+    upperLim = pi/2;
+    Res = (upperLim-lowerLim)/10;    
+    thetaUnc = lowerLim:Res:upperLim;
+else
+    thetaUnc = 0;
+end
+
+[A_c, Var_q, Var_p, n_c] = deal(zeros(length(Transmission),length(A_ps)));
 
 for i = 1:length(Transmission)
     for j = 1: length(A_ps)
         A_psVector = A_ps(j)-thickness/2:0.1:A_ps(j)+thickness/2;       
         for k = 1:length(theta) 
             PSHusimi = 0;
-            for m = 1:length(A_psVector)                     
-                PSHusimi = PSHusimi + PostselectedHusimiDisplacedThermalPhaseAveragedVac(A_psVector(m)*cos(theta(k)),...
-                    A_psVector(m)*sin(theta(k)),nTotal,r1,r2,Transmission(i),MaxQuad,Resolution);                
+            for m = 1:length(A_psVector)  
+                for u = 1:length(thetaUnc)
+                    switch state
+                        case 'PhaseAveragedDisplacedThermal'                    
+                            PSHusimi = PSHusimi + PostselectedHusimiDisplacedThermalPhaseAveragedVac(A_psVector(m)*cos(theta(k)+thetaUnc(u)),...
+                                A_psVector(m)*sin(theta(k)+thetaUnc(u)),nTotal,r1,r2,Transmission(i),MaxQuad,Resolution);
+                        case 'Thermal'
+                            PSHusimi = PSHusimi + PostselectedHusimiThermalVac(A_psVector(m)*cos(theta(k)+thetaUnc(u)),...
+                                A_psVector(m)*sin(theta(k)+thetaUnc(u)),nTotal,Transmission(i),MaxQuad,Resolution);
+                        case 'DisplacedThermal'
+      
+                            PSHusimi = PSHusimi + PostselectedHusimiDisplacedThermalVac(A_psVector(m)*cos(theta(k)+thetaUnc(u)),...
+                                A_psVector(m)*sin(theta(k)+thetaUnc(u)),nTotal,5,0,Transmission(i),MaxQuad,Resolution);
+                    end
+                end
             end
                 PSHusimi = PSHusimi / sum(sum(PSHusimi));
-                [Qx,Qy,~,~,VarQx,VarQy,PhotonNr] = ReturnQuadsHusimi( PSHusimi, MaxQuad, Resolution );
+%                 [WF] = WignerFromHusimi(PSHusimi,'PQ',-abs(MaxQuad):Resolution:abs(MaxQuad));
+%                 [Qx,Qy,~,~,VarQxWigner,VarQyWigner,PhotonNr ] = ReturnQuadsWigner( WF, MaxQuad, Resolution );
+                [Qx,Qy,~,~,~,~,VarQxWigner,VarQyWigner,PhotonNr] = ReturnQuadsHusimi( PSHusimi, MaxQuad, Resolution );
                 A_c(i,j) = A_c(i,j) + sqrt(Qx^2+Qy^2)/length(theta);
-                Var_c(i,j) = Var_c(i,j) + mean([VarQx VarQy])/length(theta);
+                Var_q(i,j) = Var_q(i,j) + VarQxWigner /length(theta);
+                Var_p(i,j) = Var_p(i,j) + VarQyWigner /length(theta);
                 n_c(i,j) = n_c(i,j) + PhotonNr/length(theta);               
         end
     end
@@ -66,24 +96,42 @@ end
 xlabel('A_{ps}');
 graphicsSettings;
 savefig(['A_c-nTotal-' num2str(nTotal) '-r1-' num2str(r1) '-r2-' num2str(r2) '-maxQuad-' ...
-    num2str(MaxQuad) '-Res-' num2str(Resolution) '.fig']);
+    num2str(MaxQuad) '-Res-' num2str(Resolution) '-phaseUncertainty-' num2str(phaseUncertainty) ...
+    '-nDisc-' num2str(nDisc) '-state-' num2str(state) '.fig']);
 
 %% Plot Variance
 figure(2);
 if length(Transmission) == 1
-    plot(A_ps,Var_c)
-    ylabel('Variance')
+    plot(A_ps,Var_q,A_ps,Var_p);
+    legend('Var_{Q}','Var_{P}','location','best');
+    ylabel('Variance (Wigner)')
+    xlabel('A_{ps}');
+    %ylim([0.5 5]);
     title(['Transmission = ' num2str(Transmission)]);
+    graphicsSettings;
 else
-    surf(A_ps,Transmission,Var_c);
+    subplot(1,2,1);
+    surf(A_ps,Transmission,Var_q);
     shading('flat');
     ylabel('Transmission');
-    zlabel('Variance');
+    zlabel('Variance_Q (Wigner)');
+    xlabel('A_{ps}');
+    %zlim([0.5 1]);
+    graphicsSettings;
+    
+    subplot(1,2,2);
+    surf(A_ps,Transmission,Var_p);
+    shading('flat');
+    ylabel('Transmission');
+    xlabel('A_{ps}');
+    zlabel('Variance_P (Wigner)');
+    %zlim([0.5 1]);
+    graphicsSettings;
 end
-xlabel('A_{ps}');
-graphicsSettings;
-savefig(['Var_c-nTotal-' num2str(nTotal) '-r1-' num2str(r1) '-r2-' num2str(r2) '-maxQuad-' ...
-    num2str(MaxQuad) '-Res-' num2str(Resolution) '.fig']);
+
+savefig(['Var-nTotal-' num2str(nTotal) '-r1-' num2str(r1) '-r2-' num2str(r2) '-maxQuad-' ...
+    num2str(MaxQuad) '-Res-' num2str(Resolution) '-phaseUncertainty-' num2str(phaseUncertainty) ...
+    '-nDisc-' num2str(nDisc) '-state-' num2str(state) '.fig']);
 
 %% Plot Photon Number
 figure(3);
@@ -100,7 +148,8 @@ end
 xlabel('A_{ps}');
 graphicsSettings;
 savefig(['n_c-nTotal-' num2str(nTotal) '-r1-' num2str(r1) '-r2-' num2str(r2) '-maxQuad-' ...
-    num2str(MaxQuad) '-Res-' num2str(Resolution) '.fig']);
+    num2str(MaxQuad) '-Res-' num2str(Resolution) '-phaseUncertainty-' num2str(phaseUncertainty) ...
+    '-nDisc-' num2str(nDisc) '-state-' num2str(state) '.fig']);
 
 
 
