1554c9da7f4d74ae4294a8a40974885053036e15
CarolinLueders <carolin.lueders@hotmail.de>
2020-09-15 16:06:53 +0200

    g2/plotNandG2Av3Ch.m: temporarily only 2 channels; logarithmic g2 plot

--- g2/plotNandG2Av3Ch.m
+++ g2/plotNandG2Av3Ch.m
@@ -10,16 +10,18 @@ function [] = plotNandG2Av3Ch(filename,parameter)
     nAvs2 = nAvs;
     g2Avs2 = g2Avs;
     g2Stds2 = g2Stds;
-    load(['AverageNandG2-X3-' filename '.mat']);
-    Is3 = Is;
-    nAvs3 = nAvs;
-    g2Avs3 = g2Avs;
-    g2Stds3 = g2Stds;
+%     load(['AverageNandG2-X3-' filename '.mat']);
+%     Is3 = Is;
+%     nAvs3 = nAvs;
+%     g2Avs3 = g2Avs;
+%     g2Stds3 = g2Stds;
     
-    errorbar(Is1, g2Avs1,g2Stds1,'ro','markerSize',7,'markerFaceColor','r','markerEdgeColor','r','linewidth',1.2); 
+    errorbar(Is1, g2Avs1,g2Stds1,'ro','markerSize',7,'markerFaceColor','r','markerEdgeColor','r','linewidth',1.2,'DisplayName','Channel 1');
+    f = gca;
+    f.XScale = 'log';
     hold on;
-    errorbar(Is2, g2Avs2,g2Stds2,'ko','markerSize',7,'markerFaceColor','k','markerEdgeColor','k','linewidth',1.2);
-    errorbar(Is3, g2Avs3,g2Stds3,'bo','markerSize',7,'markerFaceColor','b','markerEdgeColor','b','linewidth',1.2);
+    errorbar(Is2, g2Avs2,g2Stds2,'ko','markerSize',7,'markerFaceColor','k','markerEdgeColor','k','linewidth',1.2,'DisplayName','Channel 2');
+%     errorbar(Is3, g2Avs3,g2Stds3,'bo','markerSize',7,'markerFaceColor','b','markerEdgeColor','b','linewidth',1.2,'DisplayName','Channel 3');
     fontsize =22;
     ylabel('$ \langle g^{(2)}(0) \rangle $','FontSize',fontsize, 'Interpreter','latex');
     switch parameter
@@ -30,20 +32,20 @@ function [] = plotNandG2Av3Ch(filename,parameter)
         case 'delay'
             xlabel('delay','FontSize',fontsize,'Interpreter','latex');
     end
-    legend('Channel 1','Channel 2','Channel 3','location','northwest');
+    legend('location','best');
     graphicsSettings;
     ylim([0.5 2.5]);
 
     %print('g2overCurrent','-dpng');
-    print(['g2overPower-3Ch-' filename],'-dpng');
-    savefig(['g2overPower-3Ch-' filename '.fig']);
+    print(['g2overPower-allCh-' filename],'-dpng');
+    savefig(['g2overPower-allCh-' filename '.fig']);
 
     clf();
     % 
-    loglog(Is1, nAvs1,'o','LineWidth',2);
+    loglog(Is1, nAvs1,'o','LineWidth',2,'DisplayName','Channel 1');
     hold on;
-    loglog(Is2, nAvs2,'o','LineWidth',2);
-    loglog(Is3, nAvs3,'o','LineWidth',2);
+    loglog(Is2, nAvs2,'o','LineWidth',2,'DisplayName','Channel 2');
+%     loglog(Is3, nAvs3,'o','LineWidth',2,'DisplayName','Channel 3');
     ylabel('{<n>}');
     switch parameter
         case 'current'
@@ -53,8 +55,8 @@ function [] = plotNandG2Av3Ch(filename,parameter)
         case 'delay'
             xlabel('delay','FontSize',fontsize,'Interpreter','latex');
     end
-    legend('Channel 1','Channel 2','Channel 3','location','northwest');
+    legend('location','best');
     graphicsSettings
-    print(['NoverPower-3Ch-' filename],'-dpng');
-    savefig(['NoverPower-3Ch-' filename '.fig']);
+    print(['NoverPower-allCh-' filename],'-dpng');
+    savefig(['NoverPower-allCh-' filename '.fig']);
 end
