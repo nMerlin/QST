47583a1d29df903978dbd59bce71cca194a53ec5
CarolinLueders <carolin.lueders@hotmail.de>
2020-09-15 16:03:24 +0200

    Wigner/mainWigner.m: added input parser

--- Wigner/mainWigner.m
+++ Wigner/mainWigner.m
@@ -6,20 +6,33 @@ function mainWigner( nMax, mMax, varargin )
 % Options:
 % 'parallel': Use MatLabs parallel pool for higher speed
 % 'overwrite': Compute existing matrix element again
-
+%resulting matrix will be saved in the given DIRECTORY. The function will
+%be discretized in phase space from p,q = MINQ to MAXQ in steps of
+%QINTSTEP.
+%% Validate and parse input arguments
+p = inputParser;
+defaultDirectory = 'C:\Users\lab\Documents\@archived-data\Wigner';
 %directory = 'Z:\freefilesync-lab\matlab\QST\Wigner';
 %directory = 'D:\@archive\2016-08-30-wigner-test';
-directory = 'C:\Users\lab\Documents\@archived-data\Wigner';
-minq=-20;
-maxq=20;
-qintstep=0.125; %choose 1/2^n to avoid artifacts
+%directory = 'C:\Users\lab\Documents\@archived-data\Wigner-Resolution-0.25';
+addParameter(p,'Directory',defaultDirectory,@isstr);
+defaultMinq = -20;
+addParameter(p,'Minq',defaultMinq,@isnumeric);
+defaultMaxq = 20;
+addParameter(p,'Maxq',defaultMaxq,@isnumeric);
+defaultQintstep = 0.125;  %choose 1/2^n to avoid artifacts
+addParameter(p,'Qintstep',defaultQintstep,@isnumeric);
+defaultParallel = 0; %choose 1 for parallel processing
+addParameter(p,'Parallel',defaultParallel,@isnumeric);
+defaultOverwrite = false;
+addParameter(p,'Overwrite',defaultOverwrite,@islogical);
+parse(p,varargin{:});
+c = struct2cell(p.Results);
+[directory,maxq,minq,overwrite,parallel,qintstep] = c{:};
 
-parallel = 0;
-overwrite = 0;
-if nargin > 2
-    for i = 3:nargin
-        eval([varargin{i-2} '=1;']);
-    end
+%% Create directory
+if ~exist([pwd directory],'dir')
+    mkdir(directory)
 end
 
 dispstat('','init');
