f91918498afc0c1bdc9840baabb00465c662b54f
CarolinLueders <carolin.lueders@hotmail.de>
2020-10-21 16:59:34 +0200

    Unreleased/mini.m: moved to helperfunctions

--- Unreleased/mini.m
+++
@@ -1,4 +0,0 @@
-function f = mini(x, WLength, WRes,H)
-       r = sqrt(2*x(2));
-       f =  sum(sum(abs(CreateDisplacedThermalHusimiPhaseAveraged( x(1), WLength, WRes, r,'PlotOption',false)-H)));
-    end
