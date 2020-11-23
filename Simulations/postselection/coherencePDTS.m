4d8bfc52740364e89a15506c6656ad00222985c2
CarolinLueders <carolin.lueders@hotmail.de>
2020-11-23 16:54:49 +0100

    Simulations/postselection/coherencePDTS.m: 1st try

---
+++ Simulations/postselection/coherencePDTS.m
@@ -0,0 +1,8 @@
+function Coherence = coherencePDTS(nTherm,nCoherent)
+% computes coherence of a phase-averaged displaced thermal state.
+% nTherm: thermal photon number
+% nCoherent: coherent photon number
+    a = 2*nTherm + 1;
+    Coherence = (1 - exp(-2*nCoherent./a) .* besseli(0,2*nCoherent./a ) )./a;
+end
+
