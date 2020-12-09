cdba38b26c3b7169bca06c49fc3262a2f11d87b1
CarolinLueders <carolin.lueders@tu-dortmund.de>
2020-12-09 10:28:22 +0100

    Plot/graphicsSettings.m:back to old

--- Plot/graphicsSettings.m
+++ Plot/graphicsSettings.m
@@ -14,16 +14,16 @@ function [] = graphicsSettings
     set(dcmObj, 'UpdateFcn', @dataTipUpdateFcn); % increases the displayed digits of data tips 
   
     %reduce white margins around figure 
-    outerpos = ax.OuterPosition;
-    ti = ax.TightInset;
-    left = outerpos(1) + ti(1);
-    bottom = outerpos(2) + ti(2);
-    ax_width = outerpos(3)- ti(1) - ti(3);
-    ax_height = outerpos(4) - ti(2) - ti(4);
-    set(gca,'Position',[left bottom ax_width ax_height]);
-    fig = gcf;
-    fig.PaperPositionMode = 'auto';
-    fig_pos = fig.PaperPosition;
-    fig.PaperSize = [fig_pos(3) fig_pos(4)];
+%     outerpos = ax.OuterPosition;
+%     ti = ax.TightInset;
+%     left = outerpos(1) + ti(1);
+%     bottom = outerpos(2) + ti(2);
+%     ax_width = outerpos(3)- ti(1) - ti(3);
+%     ax_height = outerpos(4) - ti(2) - ti(4);
+%     set(gca,'Position',[left bottom ax_width ax_height]);
+%     fig = gcf;
+%     fig.PaperPositionMode = 'auto';
+%     fig_pos = fig.PaperPosition;
+%     fig.PaperSize = [fig_pos(3) fig_pos(4)];
 
 end
