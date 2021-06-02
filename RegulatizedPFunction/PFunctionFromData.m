20319079b9a035467020dc3b48f9b676f8235f8c
CarolinLueders <carolin.lueders@tu-dortmund.de>
2021-06-02 17:22:10 +0200

    RegulatizedPFunction/PFunctionFromData.m: with standard deviation

--- RegulatizedPFunction/PFunctionFromData.m
+++ RegulatizedPFunction/PFunctionFromData.m
@@ -1,7 +1,8 @@
-function [P] = PFunctionFromData(Xdata,thetaData,directory,xGrid,phiGrid)
+function [P,sigmaP] = PFunctionFromData(Xdata,thetaData,directory,xGrid,phiGrid)
 %calculates the regularized P function from the measured quadratures XData and 
 % phases thetaData with help of the pattern functions in directory.
 % see Jans notes, equ. 17
+%sigmaP is the standard deviation via equ. 18 
 %you can get the xGrid and phiGrid from the filenames of the archived
 %pattern funcions via 
 % [xGrid,phiGrid] = getGridsFromFilenames(directory);
@@ -11,6 +12,7 @@ xStep = min(diff(xGrid));
 phiStep = min(diff(phiGrid));
 
 sum = 0;
+sum2 = 0;
 for phi = phiGrid
     for x = xGrid 
         load(strcat(directory,'\x-',num2str(x),'-phi-',num2str(phi),'-Pattern.mat'),'pattern');
@@ -22,9 +24,14 @@ for phi = phiGrid
         N2 = length(u2(u2>0));
         weight = N/N2;
         sum = sum + weight * pattern;
+        sum2 = sum2 + weight * pattern.^2;
     end
 end
 P = sum * pi / length(phiGrid);
+P2 = sum2 * pi^2 / length(phiGrid);
+
+Ntotal = length(Xdata);
+sigmaP = sqrt((P2 - P.^2)/(Ntotal-1));
 
 end
 
