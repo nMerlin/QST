286bddf502599c683d01360cfe2cdfaffd2c8ae0
CarolinLueders <carolin.lueders@tu-dortmund.de>
2021-06-25 17:28:05 +0200

    RegulatizedPFunction/PFunctionFromData.m: improvements

--- RegulatizedPFunction/PFunctionFromData.m
+++ RegulatizedPFunction/PFunctionFromData.m
@@ -1,4 +1,4 @@
-function [P,sigmaP] = PFunctionFromData(Xdata,thetaData,directory,xGrid,phiGrid)
+function [P,sigmaP,QuadVals] = PFunctionFromData(Xdata,thetaData,directory,xGrid,phiGrid)
 %calculates the regularized P function from the measured quadratures XData and 
 % phases thetaData with help of the pattern functions in directory.
 % see Jans notes, equ. 17
@@ -11,17 +11,23 @@ function [P,sigmaP] = PFunctionFromData(Xdata,thetaData,directory,xGrid,phiGrid)
 xStep = min(diff(xGrid));
 phiStep = min(diff(phiGrid));
 
+%get the new coordinate grid from folder name  
+maxQuadToken = regexpi(directory,'maxQuad-([-0123456789.]*)-','tokens');
+maxQuad = str2double(cell2mat(maxQuadToken{1}));
+ResToken = regexpi(directory,'Resolution-([-0123456789.]*)','tokens');
+Resolution = str2double(cell2mat(ResToken{1}));
+QuadVals=-abs(maxQuad):Resolution:abs(maxQuad);
+
+% compute the P function and its uncertainty 
 sum = 0;
 sum2 = 0;
 for phi = phiGrid
     for x = xGrid 
-        load(strcat(directory,'\x-',num2str(x),'-phi-',num2str(phi),'-Pattern.mat'),'pattern');
+        load(strcat(directory,'\x-',num2str(x),'-phi-',num2str(phi),'.mat'),'pattern');
         binX = [x-xStep/2, x+xStep/2];
         binPhi = [phi-phiStep/2, phi+phiStep/2];
-        u = Xdata > min(binX) & Xdata < max(binX) & thetaData > min(binPhi) & thetaData < max(binPhi);
-        N =  length(u(u>0));
-        u2 = thetaData > min(binPhi) & thetaData < max(binPhi);
-        N2 = length(u2(u2>0));
+        N = length(Xdata(Xdata > min(binX) & Xdata < max(binX) & thetaData > min(binPhi) & thetaData < max(binPhi)));
+        N2 = length(Xdata(thetaData > min(binPhi) & thetaData < max(binPhi)));
         weight = N/N2;
         sum = sum + weight * pattern;
         sum2 = sum2 + weight * pattern.^2;
