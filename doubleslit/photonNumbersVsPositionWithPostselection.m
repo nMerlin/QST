b0d379aa7907e661262a1b820ac71c36c33b8077
CarolinLueders <carolin.lueders@hotmail.de>
2021-12-21 13:45:34 +0100

    doubleslit/photonNumbersVsPositionWithPostselection.m: work in progress

--- doubleslit/photonNumbersVsPositionWithPostselection.m
+++ doubleslit/photonNumbersVsPositionWithPostselection.m
@@ -18,7 +18,7 @@ defaultSaveTheta = false;
 addParameter(p,'SaveTheta',defaultSaveTheta,@islogical);
 defaultSaveOrth = false;
 addParameter(p,'SaveOrth',defaultSaveOrth,@islogical);
-defaultSelParams = struct('Type','fullcircle','Position',[2.5,0.5]); % use Type 'phase'?
+defaultSelParams = struct('Type','phase','Position',[0,0.2]); % use Type 'phase'?
 addParameter(p,'SelectionParameters',defaultSelParams,@isstruct);
 parse(p,varargin{:});
 c = struct2cell(p.Results);
@@ -36,14 +36,16 @@ selStr = selParamsToStr(selParams);
 [filenames,~,positions]= getParametersFromFilenames('Folder',folder,'Parameter','position');
 dispstat('','init','timestamp','keepthis',0);
 
-for i = 1:length(filenames)
+for i = 1:length(filenames)  %1
 %     load([folder '\' cell2mat(filenames(fileI))],'X2');
 %     [~,n,~] = nPhotons(X2,X2,X2);
     quantities.position(i) = positions(i);
     filename = cell2mat(filenames(i));
-    clear X1 X2 X3 theta piezoSign
-    clear O1 O2 O3 oTheta selX selTheta;
+    clear X1 X2 X3 theta piezoSign thetaMira
+    clear O1 O2 O3 oTheta oThetaMira selX selTheta thetaMiraSel;
     postFilename =  ['post-data/',filename,'-',selStr,'.mat'];
+    
+    dispstat(['loading quadratures of ' filename],'timestamp','keepthis',0);
     
     if ~saveps && ~recomputeTheta
         try
@@ -51,14 +53,22 @@ for i = 1:length(filenames)
         catch
             dispstat(['Could not find ',postFilename, ...
                 ' loading raw quadratures ...'],'timestamp','keepthis',0);
-            load([folder '\' filename],'X1','X2','X3','piezoSign');
+            load([folder '\' filename]);
             tempsaveps = true;
         end
     else
-        load([folder '\' filename],'X1','X2','X3','piezoSign');
+        load([folder '\' filename]);
     end
     
     if exist('X1','var')
+        % make sure all have the same number of pulses. 
+         if ~isequal(size(X1,1),size(X2,1),size(X3,1))
+             X1 = X1(1:min([size(X1,1),size(X2,1),size(X3,1)]),:,:);
+             X2 = X2(1:min([size(X1,1),size(X2,1),size(X3,1)]),:,:);
+             X3 = X3(1:min([size(X1,1),size(X2,1),size(X3,1)]),:,:);
+         end
+ 
+        
         quadratures = zeros([size(X1) 3]);
         quadratures(:,:,:,1) = X1;
         quadratures(:,:,:,2) = X2;
@@ -73,7 +83,8 @@ for i = 1:length(filenames)
     if ~exist('selX','var') % run only if postselection file was not loaded
         if (~exist('theta','var')) || recomputeTheta % run only if theta is not available or should be rewritten
             try
-                [theta,~] = computePhase(Xtg,XpsFast,piezoSign,'Period',periodsPerSeg);%brauchen wir die?
+%                 [theta,~] = computePhase(Xtg,XpsFast,piezoSign,'Period',periodsPerSeg);    
+                   [theta,~] = computePhase(Xtg,XpsSlow,piezoSign,'Period',periodsPerSeg); 
             catch
                 warning(['Problem using computePhase.', ...
                     ' Assigning random phase.']);
@@ -82,27 +93,49 @@ for i = 1:length(filenames)
             
         end
         
-        %select orthogonal quadratures from the postselection channels
-        if (~exist('O1','var')) || recomputeOrth 
-            [O1,O2,O3,oTheta,iOrth] = selectOrthogonal(XpsFast,XpsSlow,Xtg,theta,piezoSign);
+        if (~exist('thetaMira','var')) || recomputeTheta 
+            [thetaMira,~] = computePhase(Xtg,1,piezoSign,'Period',periodsPerSeg,'Peakthreshold',0.1); %phase of Mira from doubleslit
         end
         
+        %select orthogonal quadratures from the postselection channels
+        if (~exist('O3','var')) || recomputeOrth 
+            [O1,O2,O3,oTheta,iOrth] = selectOrthogonal(XpsFast,XpsSlow,Xtg,theta,piezoSign);
+            oThetaMira = thetaMira(iOrth);
+        end
+               
         % Compute photon numbers for each channel
         [nTg,nPsFast,nPsSlow] = nPhotons(Xtg,XpsFast,XpsSlow); 
         
         % postselection from a certain region of the Husimi function                
-        [selX,selTheta] = selectRegion(O1,O2,O3,oTheta,selParams);%,'Plot','show','Filename',[filename '-assessTheta']
+        %[selX,selTheta,iSelect] = selectRegion(O1,O2,O3,oTheta,selParams);%,'Plot','show','Filename',[filename '-assessTheta']
+        [selX,selTheta,iSelect] = selectRegionOfTotalPhase(O1,O2,O3,oTheta,selParams);%,'Plot','show','Filename',[filename '-assessTheta']
         fractionSel = length(selX(:))/length(O1(:));
         quantities.fracSel(i) = fractionSel;
         quantities.lengthSelX(i) = length(selX(:));
         quantities.lengthO1(i) = length(O1(:));
         close all;
+        thetaMiraSel = oThetaMira(iSelect);
         
     end %if ~exist selX
     
      % compute photon number of postselected quadratures in the doubleslit
-    [nDs,~,~] = nPhotons(selX,selX,selX);
+    
+    [nValues] = deal(zeros(10,1));
+    for iN=1:10
+        try
+            uniformX = seriesUniformSampling(selX,thetaMiraSel,'NBins',100); %thetaMira oder selTheta?
+        catch
+            warning(['Problem with uniformSampling.', ...
+                'Use X without uniform sampling.']);
+            uniformX = selX;
+        end  
+        [nValues(iN),~,~] = nPhotons(uniformX,uniformX,uniformX);
+
+    end 
+    
+    nDs = mean(nValues);
     quantities.nDs(i) = nDs;
+    quantities.nDsStd(i) = std(nValues);
 
     if (exist('nTg','var'))
         quantities.nTg(i) = nTg;
@@ -113,23 +146,24 @@ for i = 1:length(filenames)
      %% Save workspace variables (because recomputing them takes time)
     % Save Theta
     if savetheta 
-            save([folder '\' filename],'theta','-append');
+            save([folder '\' filename],'theta','thetaMira','-append');
     end
     
     if saveOrth
-        save([folder '\' filename],'O1','O2','O3','oTheta','iOrth','-append');
+        save([folder '\' filename],'O1','O2','O3','oTheta','oThetaMira','iOrth','-append');
     end
     
     % Save postselected variables
     if saveps || tempsaveps
         save(postFilename, ...
-            'selX','selTheta','selParams','nTg','nPsFast','nPsSlow','nDs','fractionSel');      
+            'selX','selTheta','thetaMiraSel','selParams','nTg','nPsFast','nPsSlow','nDs','fractionSel');      
         tempsaveps = false;
     end
     
 end %file iteration
 
 %% Create and write table
+save([datestr(date,'yyyy-mm-dd-'),'series3Ch-',selStr,'.mat'],'quantities');
 % Load most recent table file 'yyyy-MM-dd-series3Ch.txt'
 T = seriesRead3ChTable();
 if isempty(T)
@@ -144,7 +178,7 @@ end
 
 % write results to a new table file
 writetable(T,[datestr(date,'yyyy-mm-dd-'),'series3Ch-',selStr,'.txt']);
-save([datestr(date,'yyyy-mm-dd-'),'series3Ch-',selStr,'.mat'],'quantities');
+
 
 %% Plot stuff
 plot(positions,quantities.nDs,'o-');
