3ddcb4cb181f8415b86219ce4868f01ecd48a695
CarolinLueders <carolin.lueders@tu-dortmund.de>
2021-12-23 17:49:36 +0100

    doubleslit/DifferentPostselectionPhasesForOneDataset.m: 1st try

---
+++ doubleslit/DifferentPostselectionPhasesForOneDataset.m
@@ -0,0 +1,38 @@
+% phaseselection = -pi:0.5:pi ;
+phaseselection = 0:0.5:2*pi ;
+
+nTg = nPhotons(X2,X2,X2);
+[nDs,nDsStd] = deal(length(phaseselection));
+for i  = 1:length(phaseselection)  
+
+       selParams = struct('Type','phaseAndAmplitude','Position',[phaseselection(i),0.5,1,1]);
+      % selParams = struct('Type','phase','Position',[phaseselection(i),0.5]);
+%         [selX,selTheta,iSelect] = selectRegion(O1,O2,O3,oTheta,selParams); %,'Plot','show','Filename',['test' num2str(phaseselection(i)) '-assessTheta']);
+%        thetaMiraSel = oThetaMira(iSelect);
+        [selX,selTheta,thetaMiraSel,iSelect] = selectRegionOfTotalPhase(O1,O2,O3,oTheta,zeros(size(oThetaMira)),selParams);
+    thetaMiraSel = oThetaMira(iSelect);
+     % compute photon number of postselected quadratures in the doubleslit
+    
+    [nValues] = deal(zeros(10,1));
+    for iN=1:10
+        try
+            uniformX = seriesUniformSampling(selX,thetaMiraSel,'NBins',100); %thetaMira oder selTheta?
+        catch
+            warning(['Problem with uniformSampling.', ...
+                'Use X without uniform sampling.']);
+            uniformX = selX;
+        end  
+        [nValues(iN),~,~] = nPhotons(uniformX,uniformX,uniformX);
+
+    end 
+    
+    nDs(i) = mean(nValues);
+    nDsStd(i) = std(nValues);
+end
+
+figure(16);
+hold on;
+errorbar(phaseselection,nDs./nTg,nDsStd,'o-');
+ylim([0.8 2]);
+xlabel('postselected phase');
+ylabel('postselected photon number');
