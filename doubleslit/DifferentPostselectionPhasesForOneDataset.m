5b1b90082db89e9814285165a71de68a1a32fbd4
CarolinLueders <carolin.lueders@hotmail.de>
2022-04-28 15:51:30 +0200

    doubleslit/DifferentPostselectionPhasesForOneDataset.m: phase selection with selectionRegionOfTotalPhase or selectionRegionAroundZero instead of selectionRegion. And new label for Plots where the selection parameters are included

--- doubleslit/DifferentPostselectionPhasesForOneDataset.m
+++ doubleslit/DifferentPostselectionPhasesForOneDataset.m
@@ -1,19 +1,29 @@
-% phaseselection = -pi:0.5:pi ;
-phaseselection = 0:0.5:2*pi ;
+ %phaseselection = -pi:0.5:pi ;
+%phaseselection = 0:0.5:2*pi ;
+phaseselection = 4.5 ;
 
 % radius and thickness for amplitude selection
 r=10;
-d=1;
+d=2;
+position = 10.5;
+
+
 
 nTg = nPhotons(X2,X2,X2);
+
+[nTg,nPsFast,nPsSlow] = nPhotons(X2,X1,X3); 
+nPsMean=(nPsFast+nPsSlow)/2;
+O1norm = O1*sqrt(nPsMean/nPsFast);
+O2norm = O2*sqrt(nPsMean/nPsSlow);
+
 [nDs,nDsStd] = deal(length(phaseselection));
 for i  = 1:length(phaseselection)  
 
        selParams = struct('Type','phaseAndAmplitude','Position',[phaseselection(i),0.5,r,d]);
       % selParams = struct('Type','phase','Position',[phaseselection(i),0.5]);
-%         [selX,selTheta,iSelect] = selectRegion(O1,O2,O3,oTheta,selParams); %,'Plot','show','Filename',['test' num2str(phaseselection(i)) '-assessTheta']);
-%       
-        [selX,selTheta,iSelect] = selectRegionOfTotalPhase(O1,O2,O3,oTheta,oThetaMira,selParams);
+       % [selX,selTheta,iSelect] = selectRegion(O1norm,O2norm,O3,oTheta,selParams);%,'Plot','show','Filename',['test' num2str(phaseselection(i)) '-assessTheta']);
+       [selX,selTheta,iSelect] = selectRegionAroundZero(O1norm,O2norm,O3,oTheta,oThetaMira,selParams);
+%         [selX,selTheta,iSelect] = selectRegionOfTotalPhase(O1,O2,O3,oTheta,oThetaMira,selParams);
          thetaMiraSel = oThetaMira(iSelect);
      % compute photon number of postselected quadratures in the doubleslit
     
@@ -38,10 +48,9 @@ end
 %nDs_amplitudeSelection 
 selParams2 = struct('Type','fullcircle','Position',[r,d]);
       % selParams = struct('Type','phase','Position',[phaseselection(i),0.5]);
-%         [selX,selTheta,iSelect] = selectRegion(O1,O2,O3,oTheta,selParams); %,'Plot','show','Filename',['test' num2str(phaseselection(i)) '-assessTheta']);
-%       
-        [selX,selTheta,iSelect] = selectRegionOfTotalPhase(O1,O2,O3,oTheta,oThetaMira,selParams2);
-         thetaMiraSel = oThetaMira(iSelect);
+        [selX,selTheta,iSelect2] = selectRegionOfTotalPhase(O1,O2,O3,oTheta,oThetaMira,selParams2);
+  
+         thetaMiraSel = oThetaMira(iSelect2);
      % compute photon number of postselected quadratures in the doubleslit
     
     [nValues] = deal(zeros(10,1));
@@ -60,9 +69,11 @@ selParams2 = struct('Type','fullcircle','Position',[r,d]);
     nDs_amplSel = mean(nValues);
     nDsStd_amplSel = std(nValues);
     %%
-figure(16);
+figure(5);
 hold on;
-errorbar(phaseselection,nDs./nDs_amplSel,nDsStd,'o-');
-ylim([0.8 2]);
+%errorbar(phaseselection,nDs./nTg,nDsStd,'o-','DisplayName',['Husimi+theta-thetaMira-pos-' num2str(position) '-r-' num2str(r) '-d-' num2str(d)]);
+errorbar(phaseselection,nDs./nDs_amplSel,nDsStd,'o-','DisplayName',['Husimi+theta-thetaMira(withoutPi)-pos-' num2str(position) '-r-' num2str(r) '-d-' num2str(d)]);
+legend();
+ylim([0.5 2]);
 xlabel('postselected phase');
 ylabel('postselected photon number');
