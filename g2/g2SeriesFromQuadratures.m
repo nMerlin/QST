56b60e3b7756186ad4b5d18b620029bfa6e504d0
CarolinLueders <carolin.lueders@tu-dortmund.de>
2022-05-09 17:05:48 +0200

    g2/g2SeriesFromQuadratures.m: added parameter case current

--- g2/g2SeriesFromQuadratures.m
+++ g2/g2SeriesFromQuadratures.m
@@ -54,6 +54,10 @@ for iStruct =  1:length(Contents)
     % currentToken = regexpi(filename,'mat([0123456789,]*)','tokens');
     
     switch parameter
+        case 'current'
+            currentToken = regexpi(filename,'([0123456789,]*)mA','tokens');
+             currentToken{1}=strrep(currentToken{1},',','.');
+             dataStruct(iStruct).I = str2double(cell2mat(currentToken{1}));
         case 'power'    
             %currentToken = regexpi(filename,'([0123456789,]*)mW','tokens');
             currentToken = regexpi(filename,'([0123456789,]*)mW-4mW','tokens');
